f2fe737178429836221773f41e1d8389169760ef
Allen Robel <arobel@cisco.com>
2022-09-27 16:13:41 -0700

    Use lib.common netbox() for instantiation, more...
    Add --detail option

--- role_print.py
+++ role_print.py
@@ -3,15 +3,17 @@
 Name: role_print.py
 Description: Print information about a device role
 '''
-our_version = 100
+our_version = 101
 import argparse
-import pynetbox
-from lib.credentials import NetboxCredentials
+import json
+from lib.common import netbox
 from lib.colors import rgb_to_color
 
+help_detail = 'Optional. If present, print detailed info about role.'
 help_role = 'Role for the device.'
 
 ex_prefix     = 'Example: '
+ex_detail = '{} --detail'.format(ex_prefix)
 ex_role = '{} --role leaf'.format(ex_prefix)
 
 parser = argparse.ArgumentParser(
@@ -20,6 +22,12 @@ parser = argparse.ArgumentParser(
 mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
 default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
 
+default.add_argument('--detail',
+                     dest='detail',
+                     required=False,
+                     default=False,
+                     action='store_true',
+                     help=help_detail + ex_detail)
 mandatory.add_argument('--role',
                      dest='role',
                      required=True,
@@ -46,13 +54,20 @@ def get_device_role():
         error()
     return role
 
+def print_detail():
+    pretty = json.dumps(dict(role), indent=4, sort_keys=True)
+    print(pretty)
+
 def print_headers():
     print(fmt.format('id', 'role_name', 'device_count', 'rgb', 'color', 'vm_role', 'description'))
     print(fmt.format('-' * 5, '-' * 15, '-' * 12, '-' * 6, '-' * 15, '-' * 7, '-' * 30))
 
-nc = NetboxCredentials()
-nb = pynetbox.api(nc.url, token=nc.token)
+nb = netbox()
 
 role = get_device_role()
+if cfg.detail:
+    print_detail()
+    exit()
+
 print_headers()
 print(fmt.format(role.id, role.name, role.device_count, role.color, rgb_to_color(role.color), role.vm_role, role.description))

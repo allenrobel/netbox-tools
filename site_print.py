dadc918284692c16feded334afbca05208aa2b81
Allen Robel <arobel@cisco.com>
2022-09-27 16:22:49 -0700

    Use lib.common netbox() for instantiation

--- site_print.py
+++ site_print.py
@@ -3,14 +3,16 @@
 Name: site_print.py
 Description: Print information about a site
 '''
-our_version = 100
-import pynetbox
+our_version = 101
 import argparse
-from lib.credentials import NetboxCredentials
+import json
+from lib.common import netbox
 
+help_detail = 'Optional. If present, print detailed info about role.'
 help_site = 'Retrieve information for site'
 
 ex_prefix     = 'Example: '
+ex_detail = '{} --detail'.format(ex_prefix)
 ex_site = '{} --site fabric_1'.format(ex_prefix)
 
 parser = argparse.ArgumentParser(
@@ -19,6 +21,12 @@ parser = argparse.ArgumentParser(
 mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
 default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
 
+default.add_argument('--detail',
+                     dest='detail',
+                     required=False,
+                     default=False,
+                     action='store_true',
+                     help=help_detail + ex_detail)
 mandatory.add_argument('--site',
                      dest='site',
                      required=True,
@@ -45,12 +53,19 @@ def get_site():
         error()
     return site
 
+def print_detail():
+    pretty = json.dumps(dict(site), indent=4, sort_keys=True)
+    print(pretty)
+
 def print_headers():
     print(fmt.format('id', 'name', 'device_count', 'rack_count', 'prefix_count', 'status', 'description'))
     print(fmt.format('-' * 5, '-' * 15, '-' * 12, '-' * 12, '-' * 12, '-' * 10, '-' * 30))
 
-nc = NetboxCredentials()
-nb = pynetbox.api(nc.url, token=nc.token)
+nb = netbox()
 site = get_site()
+if cfg.detail:
+    print_detail()
+    exit()
+
 print_headers()
 print(fmt.format(site.id, site.name, site.device_count, site.rack_count, site.prefix_count, site.status.value, site.description))

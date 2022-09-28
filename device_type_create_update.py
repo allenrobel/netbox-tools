9bd3720116ec3bbb9b5ef0718dacc0776d79dee9
Allen Robel <arobel@cisco.com>
2022-09-28 12:03:43 -0700

    Add tags argument

--- device_type_create_update.py
+++ device_type_create_update.py
@@ -5,18 +5,19 @@ Description: Create or update a Netbox device type.
 '''
 our_version = 102
 import argparse
+import re
 from lib.common import netbox, load_yaml
 from lib.device_type import DeviceType
 
 help_comments = 'Freeform comments for this device type.'
 help_manufacturer = 'Who makes this device type'
-help_model = 'Typically, the product/model number for this device'
-
+help_model = 'Typically, the product/model number for this device type'
+help_tags = 'A comma-separated list of tags for this device type (these must all exist in netbox already)'
 ex_prefix     = 'Example: '
 ex_comments = '{} --comments "36x40/100G QSFP28 Ethernet Module"'.format(ex_prefix)
 ex_manufacturer = '{} --manufacturer cisco'.format(ex_prefix)
 ex_model = '{} --model N9K-C9336C-FX2'.format(ex_prefix)
-
+ex_tags = '{} --tags admin,infra'
 parser = argparse.ArgumentParser(
          description='DESCRIPTION: Create or update a Netbox device type (Netbox device type is roughly equivilent to model number)')
 
@@ -39,6 +40,12 @@ mandatory.add_argument('--model',
                      required=True,
                      help=help_model + ex_model)
 
+optional.add_argument('--tags',
+                     dest='tags',
+                     required=False,
+                     default=None,
+                     help=help_tags + ex_tags)
+
 parser.add_argument('--version',
                     action='version',
                     version='%(prog)s {}'.format(our_version))
@@ -52,6 +59,8 @@ info['manufacturer'] = cfg.manufacturer
 info['model'] = cfg.model
 if cfg.comments != None:
     info['comments'] = cfg.comments
+if cfg.tags != None:
+    info['tags'] = re.split(',', cfg.tags)
 
 d = DeviceType(nb, info)
 d.create_or_update()

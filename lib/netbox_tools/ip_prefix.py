17292ba95f2578c687c3d1883caab7680192b94a
Allen Robel <arobel@cisco.com>
2022-11-29 10:54:47 -1000

    run through black and pylint

--- lib/netbox_tools/ip_prefix.py
+++ lib/netbox_tools/ip_prefix.py
@@ -1,140 +1,220 @@
-'''
+"""
 Name: ip_prefix.py
-Description: Class for create, update, delete operations on netbox ip_prefix
-'''
-
-from netbox_tools.common import device_id, get_device
+Description: Create, update, delete operations on netbox ip_prefix
+"""
+from inspect import stack
+import sys
 from netbox_tools.common import site_id, vlan_vid_to_id
 
-class IpPrefix(object):
-    def __init__(self, nb, info):
-        self.nb = nb
-        self.info = info
-        self.args = dict()
-        self.mandatory_keys = ['prefix']
-        self.optional_keys = ['description', 'site', 'status', 'vlan', 'tags']
-        self.default_status = 'active'
-        self.validate_keys()
-        self.generate_args()
+OUR_VERSION = 102
 
-    def validate_keys(self):
-        for key in self.mandatory_keys:
-            if key not in self.info:
-                print('ip_address.validate_keys: exiting. mandatory key {} not found in info {}'.format(key, self.info))
-                exit(1)
 
-    def generate_args(self):
-        self.args['prefix'] = self.prefix
-        if self.description != None:
-            self.args['description'] = self.description
-        if self.site != None:
-            if self._site_id == None:
-                print('IpPrefix.generate_args: exiting. prefix {} site {} does not exist in netbox. Either create the site first, or do not specify a site for this prefix.'.format(
-                    self.prefix,
-                    self.site))
-                exit(1)
-            self.args['site'] = self._site_id
-        if self.status != None:
-            self.args['status'] = self.status
+class IpPrefix:
+    """
+    Create, update, delete operations on netbox ip_prefix
+    """
+
+    def __init__(self, netbox_obj, info):
+        self.lib_version = OUR_VERSION
+        self._classname = __class__.__name__
+        self._netbox_obj = netbox_obj
+        self._info = info
+        self._args = {}
+        self._mandatory_keys = set()
+        self._mandatory_keys.add("prefix")
+        self._optional_keys = set()
+        self._optional_keys.add("description")
+        self._optional_keys.add("site")
+        self._optional_keys.add("status")
+        self._optional_keys.add("vlan")
+        self._optional_keys.add("tags")
+        self.default_status = "active"
+        self._validate_keys()
+        self._generate_args()
+
+    def log(self, *args):
+        """
+        simple logger
+        """
+        print(
+            f"{self._classname}(v{self.lib_version}).{stack()[1].function}: {' '.join(args)}"
+        )
+
+    def _validate_keys(self):
+        """
+        Verify that all mandatory create/update operation keys are set.
+        If all keys are not set, log an error and exit.
+        """
+        for key in self._mandatory_keys:
+            if key not in self._info:
+                self.log(f"exiting. mandatory key {key} not found in info {self._info}")
+                sys.exit(1)
+
+    def _generate_args(self):
+        """
+        Generate all supported arguments for create and update methods
+        """
+        self._args["prefix"] = self.prefix
+        if self.description is not None:
+            self._args["description"] = self.description
+        if self.site is not None:
+            if self._site_id is None:
+                self.log(
+                    f"exiting. prefix {self.prefix} site {self.site} does not exist in netbox.",
+                    "Either create the site first, or do not specify a site for this prefix.",
+                )
+                sys.exit(1)
+            self._args["site"] = self._site_id
+        if self.status is not None:
+            self._args["status"] = self.status
         else:
-            self.args['status'] = self.default_status
-        if self.vlan != None:
-            self.args['vlan'] = self.vlan_id
+            self._args["status"] = self.default_status
+        if self.vlan is not None:
+            self._args["vlan"] = self.vlan_id
 
     def delete(self):
-        print('IpPrefix.delete: prefix {}'.format(self.prefix))
-        #self.args['id'] = self.prefix_id
-        if self.prefix_object == None:
-            print('IpPrefix.delete: Nothing to do. Prefix {} does not exist in netbox'.format(self.prefix))
+        """
+        delete a prefix
+        """
+        self.log(f"prefix {self.prefix}")
+        if self.prefix_object is None:
+            self.log(f"Nothing to do. Prefix {self.prefix} does not exist in netbox")
             return
         try:
             self.prefix_object.delete()
-        except Exception as e:
-            print('IpPrefix.delete: Exiting. Unable to delete prefix {}.  Error was: {}'.format(
-                self.prefix, e))
-            exit(1)
+        except Exception as _general_exception:
+            self.log(
+                f"Exiting. Unable to delete prefix {self.prefix}",
+                f"Exception detail: {_general_exception}",
+            )
+            sys.exit(1)
 
     def create(self):
-        print('IpPrefix.create: prefix {}'.format(self.prefix))
+        """
+        create a prefix
+        """
+        self.log(f"prefix {self.prefix}")
         try:
-            self.nb.ipam.prefixes.create(self.args)
-        except Exception as e:
-            print('IpPrefix.create: Exiting. Unable to create prefix {}.  Error was: {}'.format(
-                self.prefix,
-                e))
-            exit(1)
+            self._netbox_obj.ipam.prefixes.create(self._args)
+        except Exception as _general_exception:
+            self.log(
+                f"Exiting. Unable to create prefix {self.prefix}",
+                f"Exception detail: {_general_exception}",
+            )
+            sys.exit(1)
 
     def update(self):
-        print('IpPrefix.update: prefix {}'.format(self.prefix))
-        self.args['id'] = self.prefix_id
+        """
+        update a prefix
+        """
+        self.log(f"prefix {self.prefix}")
+        self._args["id"] = self.prefix_id
         try:
-            self.prefix_object.update(self.args)
-        except Exception as e:
-            print('IpPrefix.update: Exiting. Unable to update prefix {}.  Error was: {}'.format(
-                self.prefix, e))
-            exit(1)
+            self.prefix_object.update(self._args)
+        except Exception as _general_exception:
+            self.log(
+                f"Exiting. Unable to update prefix {self.prefix}",
+                f"Exception detail: {_general_exception}",
+            )
+            sys.exit(1)
 
     def create_or_update(self):
-        if self.prefix_object == None:
+        """
+        entry point into create and update methods
+        """
+        if self.prefix_object is None:
             self.create()
         else:
             self.update()
 
     @property
     def description(self):
-        if 'description' in self.info:
-            return self.info['description']
-        else:
-            return None
+        """
+        Return the prefix description set by the caller.
+        If the caller didn't set this, return None.
+        """
+        if "description" in self._info:
+            return self._info["description"]
+        return None
 
     @property
     def prefix_object(self):
-        return self.nb.ipam.prefixes.get(prefix=self.prefix)
+        """
+        Return the netbox prefix object associated with the
+        prefix set by the caller. If the caller didn't set prefix,
+        this is caught in _validate_keys, so no need here for error handling.
+        """
+        return self._netbox_obj.ipam.prefixes.get(prefix=self.prefix)
 
     @property
     def prefix(self):
-        return self.info['prefix']
+        """
+        Return the prefix set by the caller. If the caller didn't set prefix,
+        this is caught in _validate_keys, so no need here for error handling.
+        """
+        return self._info["prefix"]
 
     @property
     def prefix_id(self):
-        if self.prefix_object == None:
-            print('IpPrefix.prefix_id: exiting. Unable to retrieve prefix {}'.format(self.prefix))
-            exit(1)
+        """
+        Return the netbox prefix ID associated with the
+        prefix set by the caller. If the caller didn't set prefix,
+        this is caught in _validate_keys, so no need here for error handling.
+        """
+        if self.prefix_object is None:
+            self.log(
+                f"exiting. Unable to retrieve prefix {self.prefix}",
+                "Prefix was not found in your Netbox instance.",
+            )
+            sys.exit(1)
         return self.prefix_object.id
 
     @property
     def site(self):
-        if 'site' in self.info:
-            return self.info['site']
-        else:
-            return None
+        """
+        Return the site set by the caller.
+        If the caller didn't set this, return None.
+        """
+        if "site" in self._info:
+            return self._info["site"]
+        return None
 
     @property
     def _site_id(self):
-        if 'site' in self.info:
-            return site_id(self.nb, self.site)
-        else:
-            return None
+        """
+        Return the Netbox site ID associated with site.
+        If the caller didn't set site, return None.
+        """
+        if self.site is not None:
+            return site_id(self._netbox_obj, self.site)
+        return None
 
     @property
     def status(self):
-        if 'status' in self.info:
-            return self.info['status']
-        else:
-            return None
+        """
+        Return the status set by the caller.
+        If the caller didn't set this, return None.
+        """
+        if "status" in self._info:
+            return self._info["status"]
+        return None
 
     @property
     def vlan(self):
-        '''Vlan vid'''
-        if 'vlan' in self.info:
-            return self.info['vlan']
-        else:
-            return None
+        """
+        Return the vlan by the caller.
+        If the caller didn't set this, return None.
+        """
+        if "vlan" in self._info:
+            return self._info["vlan"]
+        return None
 
     @property
     def vlan_id(self):
-        '''Netbox ID of the vlan'''
-        if 'vlan' in self.info:
-            return vlan_vid_to_id(self.nb, self.vlan)
-        else:
-            return None
+        """
+        Return the Netbox ID of the vlan set by the caller.
+        If the caller didn't set vlan, return None.
+        """
+        if "vlan" in self._info:
+            return vlan_vid_to_id(self._netbox_obj, self.vlan)
+        return None

2a67139dbc385eb0078d82a6cf07cc888359ecf4
Allen Robel <arobel@cisco.com>
2022-12-01 12:47:58 -1000

    run thru pylint/black

--- scripts/cable_delete_one.py
+++ scripts/cable_delete_one.py
@@ -1,86 +1,108 @@
 #!/usr/bin/env python3
-'''
-Name: cable_create_update_one.py
+"""
+Name: cable_delete_one.py
 Description: Delete cable from Netbox with key ``--key`` in file ``--yaml``
-'''
-our_version = 100
+"""
 import argparse
+from inspect import stack
+import sys
 from netbox_tools.common import netbox, load_yaml
 from netbox_tools.cable import Cable
 
-def get_parser():
-    help_yaml = 'YAML file containing cable information.'
-    help_key = 'Key to delete.'
+OUR_VERSION = 101
 
-    ex_prefix     = 'Example: '
-    ex_yaml = '{} --yaml ./cables.yml'.format(ex_prefix)
-    ex_key = '{} --key mycable '.format(ex_prefix)
+
+def get_parser():
+    """
+    return an argparse instance
+    """
+    help_yaml = "YAML file containing cable information."
+    help_key = "Key to delete."
+
+    ex_prefix = "Example: "
+    ex_yaml = f"{ex_prefix} --yaml ./cables.yml"
+    ex_key = f"{ex_prefix} --key mycable"
 
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Delete cable from Netbox with key ``--key`` in file ``--yaml``')
+        description="DESCRIPTION: Delete cable from Netbox with key ``--key`` in file ``--yaml``"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
 
-    mandatory.add_argument('--yaml',
-                        dest='yaml',
-                        required=True,
-                        help=help_yaml + ex_yaml)
+    mandatory.add_argument(
+        "--yaml", dest="yaml", required=True, help=help_yaml + ex_yaml
+    )
 
-    mandatory.add_argument('--key',
-                        dest='key',
-                        required=True,
-                        help=help_key + ex_key)
+    mandatory.add_argument("--key", dest="key", required=True, help=help_key + ex_key)
 
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(our_version))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
-def get_device_from_interface_key(info, interface_key):
-    if interface_key not in info['interfaces']:
-        print('get_device_port_from_port_key: exiting. port_key {} not found in interfaces {}'.format(
-            ','.join(info['interfaces'].keys())
-        ))
-        exit(1)
-    if 'device' not in info['interfaces'][interface_key]:
-        print('get_device_port_from_port_key: exiting. device key not found in interface dict {}'.format(
-            info['interfaces'][interface_key]
-        ))
-        exit(1)
-    return info['interfaces'][interface_key]['device']
 
-def get_interface_from_interface_key(info, interface_key):
-    if interface_key not in info['interfaces']:
-        print('get_device_port_from_port_key: exiting. port_key {} not found in interfaces {}'.format(
-            ','.join(info['interfaces'].keys())
-        ))
-        exit(1)
-    if 'interface' not in info['interfaces'][interface_key]:
-        print('get_device_port_from_port_key: exiting. interface key not found in interface dict {}'.format(
-            info['interfaces'][interface_key]
-        ))
-        exit(1)
-    return info['interfaces'][interface_key]['interface']
+def log(*args):
+    """
+    simple logger
+    """
+    print(f"{stack()[1].function}(v{OUR_VERSION}): {' '.join(args)}")
 
-def make_args(info, key):
-    args = dict()
-    args['port_a_type'] = 'interface'
-    args['port_b_type'] = 'interface'
-    args['port_a'] = get_interface_from_interface_key(info, info['cables'][key]['port_a'])
-    args['port_b'] = get_interface_from_interface_key(info, info['cables'][key]['port_b'])
-    args['device_a'] = get_device_from_interface_key(info, info['cables'][key]['port_a'])
-    args['device_b'] = get_device_from_interface_key(info, info['cables'][key]['port_b'])
-    args['label'] = info['cables'][key]['label']
+
+def get_device_from_interface_key(interface_key):
+    """
+    given an interface key, return the associated device name
+    """
+    if interface_key not in info["interfaces"]:
+        interfaces = ",".join(info["interfaces"].keys())
+        log(f"exiting. port_key {interface_key} not found in interfaces {interfaces}")
+        sys.exit(1)
+    if "device" not in info["interfaces"][interface_key]:
+        log(
+            "exiting.",
+            f"device key not found in interface dict {info['interfaces'][interface_key]}",
+        )
+        sys.exit(1)
+    return info["interfaces"][interface_key]["device"]
+
+
+def get_interface_from_interface_key(interface_key):
+    """
+    given an interface key, return the associated interface name
+    """
+    if interface_key not in info["interfaces"]:
+        interfaces = ",".join(info["interfaces"].keys())
+        log(f"exiting. port_key {interface_key} not found in interfaces {interfaces}")
+        sys.exit(1)
+    if "interface" not in info["interfaces"][interface_key]:
+        log(
+            "exiting.",
+            f"interface key not found in interface dict {info['interfaces'][interface_key]}",
+        )
+        sys.exit(1)
+    return info["interfaces"][interface_key]["interface"]
+
+
+def make_args(key):
+    """
+    return the arguments dictionary expected by Cable()
+    """
+    args = {}
+    args["port_a_type"] = "interface"
+    args["port_b_type"] = "interface"
+    args["port_a"] = get_interface_from_interface_key(info["cables"][key]["port_a"])
+    args["port_b"] = get_interface_from_interface_key(info["cables"][key]["port_b"])
+    args["device_a"] = get_device_from_interface_key(info["cables"][key]["port_a"])
+    args["device_b"] = get_device_from_interface_key(info["cables"][key]["port_b"])
+    args["label"] = info["cables"][key]["label"]
     return args
 
+
 cfg = get_parser()
-nb = netbox()
+netbox_obj = netbox()
 info = load_yaml(cfg.yaml)
-if cfg.key not in info['cables']:
-    print('exiting. Nothing to do.  key {} not found in yaml {}'.format(cfg.key, cfg.yaml))
-    exit()
-args = make_args(info, cfg.key)
-cable = Cable(nb, args)
+if cfg.key not in info["cables"]:
+    log(f"exiting. Nothing to do. key {cfg.key} not found in yaml {cfg.yaml}")
+    sys.exit(0)
+cable = Cable(netbox_obj, make_args(cfg.key))
 cable.delete()

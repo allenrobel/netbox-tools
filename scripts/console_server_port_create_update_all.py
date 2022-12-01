4d3cf5a93f66ed002b699e2b5dca0803d5ff061f
Allen Robel <arobel@cisco.com>
2022-12-01 13:38:04 -1000

    run thru pylint/black

--- scripts/console_server_port_create_update_all.py
+++ scripts/console_server_port_create_update_all.py
@@ -1,39 +1,47 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: console_server_port_create_update_all.py
-Description: Create or update operations on Netbox endpoint /dcim/console-server-ports/ from information in a YAML file.
-'''
-our_version = 102
+Description: create/update netbox console-server-ports from information in a YAML file.
+"""
 import argparse
 from netbox_tools.common import netbox, load_yaml
 from netbox_tools.console_server_port import ConsoleServerPort
 
-def get_parser():
-    help_yaml = 'YAML file containing console_server_ports information.'
+OUR_VERSION = 102
 
-    ex_prefix     = 'Example: '
-    ex_yaml = '{} --yaml ./console_server_ports.yml'.format(ex_prefix)
+
+def get_parser():
+    """
+    return an argparse parser object
+    """
+    help_yaml = "YAML file containing console_server_ports information."
+
+    ex_prefix = "Example: "
+    ex_yaml = f"{ex_prefix} --yaml ./console_server_ports.yml"
 
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Create or update operations on Netbox endpoint /dcim/console-server-ports/ from information in a YAML file')
+        description="DESCRIPTION: create/update netbox console-server-ports"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
 
-    mandatory.add_argument('--yaml',
-                        dest='yaml',
-                        required=True,
-                        help=help_yaml + ex_yaml)
+    mandatory.add_argument(
+        "--yaml", dest="yaml", required=True, help=f"{help_yaml} {ex_yaml}"
+    )
 
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(our_version))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
+
 cfg = get_parser()
-nb = netbox()
+netbox_obj = netbox()
 info = load_yaml(cfg.yaml)
-for key in info['console_server_ports']:
-    console_server_port = ConsoleServerPort(nb, info['console_server_ports'][key])
+for key in info["console_server_ports"]:
+    console_server_port = ConsoleServerPort(
+        netbox_obj,
+        info["console_server_ports"][key]
+    )
     console_server_port.create_or_update()

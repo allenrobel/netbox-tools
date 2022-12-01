3bb40b7804d82ddc90461850dbe3908542faf4a4
Allen Robel <arobel@cisco.com>
2022-12-01 12:30:09 -1000

    Remove unused imports/code, improve logging, more
    device.py and virtual_machine.py were both changed recently to map primary ip to device/vm.  With this change, we no longer have to do this in scripts.
    
    Also, run though pylint and black.

--- scripts/entity_create_update_all.py
+++ scripts/entity_create_update_all.py
@@ -11,27 +11,19 @@ all entities within the YAML file pointed to with --yaml
 See info.yml in the top-level directory of this repo for the YAML structure this script assumes.
 """
 import argparse
+from inspect import stack
 import sys
 
-# local libraries
 from netbox_tools.cluster import Cluster
 from netbox_tools.cluster_type import ClusterType
 from netbox_tools.common import (
     netbox,
-    interface_id,
-    ip_address_id,
-    virtual_interface_id,
     load_yaml,
-    make_ip_address_dict
+    make_ip_address_dict,
 )
 from netbox_tools.console_port import ConsolePort
 from netbox_tools.console_server_port import ConsoleServerPort
-from netbox_tools.device import (
-    Device,
-    initialize_device_primary_ip,
-    make_device_primary_ip,
-    map_device_primary_ip
-)
+from netbox_tools.device import Device
 from netbox_tools.device_type import DeviceType
 from netbox_tools.interface import Interface
 from netbox_tools.ip_address import IpAddress
@@ -44,16 +36,11 @@ from netbox_tools.site import Site
 from netbox_tools.tag import Tag
 from netbox_tools.virtual_interface import VirtualInterface
 from netbox_tools.virtual_ip_address import VirtualIpAddress
-from netbox_tools.virtual_machine import (
-    VirtualMachine,
-    initialize_vm_primary_ip,
-    make_vm_primary_ip,
-    map_vm_primary_ip
-)
+from netbox_tools.virtual_machine import VirtualMachine
 from netbox_tools.vlan import Vlan
 from netbox_tools.vlan_group import VlanGroup
 
-OUR_VERSION = 105
+OUR_VERSION = 106
 
 
 def get_parser():
@@ -80,75 +67,32 @@ def get_parser():
     return parser.parse_args()
 
 
-# Used for backward-compatibility. Remove after 2023-09-29
+def log(*args):
+    """
+    simple logger
+    """
+    print(f"{stack()[1].function}(v{OUR_VERSION}): {' '.join(args)}")
+
+
 def fix_deprecations():
     """
     A couple parameter names have changed. If the user is using the older
     parameter names, warn them and copy the values to the new names.
+
+    TODO: Used for backward-compatibility. Remove after 2023-09-29
     """
     if "mgmt_interface" in info:
-        print(
-            "device_create_update_all.fix_deprecations: \
-            WARNING: devices: <device>: mgmt_interface in your YAML file is deprecated. \
-            Use devices: <device>: interface instead."
+        log(
+            "WARNING: devices: <device>: mgmt_interface in your YAML file is deprecated.",
+            "Use devices: <device>: interface instead.",
         )
         info["interface"] = info["mgmt_interface"]
     if "name" in info:
-        print(
-            "device_create_update_all.fix_deprecations: \
-            WARNING: devices: <device>: name in your YAML file is deprecated. \
-            Use devices: <device>: device instead."
+        log(
+            "WARNING: devices: <device>: name in your YAML file is deprecated.",
+            "Use devices: <device>: device instead.",
         )
         info["device"] = info["name"]
-
-
-def assign_primary_ip_to_device(ip4, device, interface):
-    """
-    call three functions that, together, set the primary_ip for
-    a netbox device.
-    """
-    ipv4_id = ip_address_id(nb, ip4)
-    intf_id = interface_id(nb, device, interface)
-    if ipv4_id is None:
-        print(
-            f"assign_primary_ip_to_device: \
-            Exiting. Address {ip4} not found in netbox"
-        )
-        sys.exit(1)
-    if intf_id is None:
-        print(
-            f"assign_primary_ip_to_device: Exiting. \
-            device {device} interface {interface} not found in netbox"
-        )
-        sys.exit(1)
-    initialize_device_primary_ip(nb, device)
-    map_device_primary_ip(nb, device, interface, ip4)
-    make_device_primary_ip(nb, device, ip4)
-
-
-def assign_primary_ip_to_vm(ip4, virtual_machine, interface):
-    """
-    call three functions that, together, set the primary_ip for
-    a netbox virtual machine.
-    """
-    ip4_id = ip_address_id(nb, ip4)
-    intf_id = virtual_interface_id(nb, virtual_machine, interface)
-    if ip4_id is None:
-        print(f"assign_primary_ip_to_vm: Exiting. Address {ip4} not found in netbox")
-        sys.exit(1)
-    if intf_id is None:
-        print(
-            f"assign_primary_ip_to_vm: \
-            Exiting. virtual_machine {virtual_machine} interface {interface} not found in netbox"
-        )
-        sys.exit(1)
-    print(
-        f"assign_primary_ip_to_vm: \
-        vm {virtual_machine} interface {interface} ip {ip4} ip4_id {ip4_id}"
-    )
-    initialize_vm_primary_ip(nb, virtual_machine)
-    map_vm_primary_ip(nb, virtual_machine, interface, ip4)
-    make_vm_primary_ip(nb, virtual_machine, ip4)
 
 
 def get_interface_dict(device_dict, interfaces_dict):
@@ -158,69 +102,67 @@ def get_interface_dict(device_dict, interfaces_dict):
     from the interfaces_dict.
     """
     if "interface" not in device_dict:
-        print(
-            f"get_interface: exiting. interface key not found in device_dict {device_dict}"
-        )
+        log(f"exiting. interface key not found in device_dict {device_dict}")
         sys.exit(1)
     interface_key = device_dict["interface"]
     if interface_key not in interfaces_dict:
-        print(
-            f"get_interface: exiting. \
-            Interface {interface_key} not found in {cfg.yaml} interfaces {interfaces_dict.keys()}."
+        log(
+            "exiting",
+            f"Interface {interface_key} not found in {cfg.yaml}",
+            f"interfaces dictionary: {interfaces_dict.keys()}.",
         )
         sys.exit(1)
     return interfaces_dict[interface_key]
 
 
-def devices_create_update(interfaces_dict, ip_addresses_dict, devices_dict, device_class):
+def devices_create_update(
+    interfaces_dict, ip_addresses_dict, devices_dict, device_class
+):
     """
     create or update device and set the device's primary_ip to
     its management interface ipv4 address
     """
     for device_key in devices_dict:
-        device_obj = device_class(nb, devices_dict[device_key])
+        device_obj = device_class(netbox_obj, devices_dict[device_key])
         device_obj.create_or_update()
         interface_dict = get_interface_dict(devices_dict[device_key], interfaces_dict)
         if "ip4" not in interface_dict:
-            print(
-                f"device {interface_dict['device']} interface {interface_dict['interface']}, \
-                skipping ipv4 address processing since ip4 key is missing"
+            log(
+                f"device {interface_dict['device']} interface {interface_dict['interface']}",
+                "skipping ipv4 address processing since ip4 key is missing",
             )
             continue
-        intf_obj = Interface(nb, interface_dict)
+        intf_obj = Interface(netbox_obj, interface_dict)
         intf_obj.create_or_update()
         ip_address_dict = make_ip_address_dict(ip_addresses_dict, interface_dict)
-        ip4_obj = IpAddress(nb, ip_address_dict)
+        ip4_obj = IpAddress(netbox_obj, ip_address_dict)
         ip4_obj.create_or_update()
-        print("---")
+        log("---")
 
 
-def virtual_machines_create_update(interfaces_dict, ip_addresses_dict, vm_dict, virtual_machine_class):
+def virtual_machines_create_update(
+    interfaces_dict, ip_addresses_dict, vm_dict, virtual_machine_class
+):
     """
     create or update virtual_machine and set the virtual_machine's
     primary_ip to its management interface ipv4 address
     """
     for vm_key in vm_dict:
-        vm_obj = virtual_machine_class(nb, vm_dict[vm_key])
+        vm_obj = virtual_machine_class(netbox_obj, vm_dict[vm_key])
         vm_obj.create_or_update()
         interface_dict = get_interface_dict(vm_dict[vm_key], interfaces_dict)
         if "ip4" not in interface_dict:
-            print(
-                f"virtual_machine {interface_dict['virtual_machine']} \
-                interface {interface_dict['interface']}, \
-                skipping ipv4 address processing since ip4 key is missing"
+            log(
+                "skipping ipv4 address processing since ip4 key is missing:",
+                f"virtual_machine {interface_dict['virtual_machine']}",
+                f"interface {interface_dict['interface']}",
             )
             continue
-        intf_obj = VirtualInterface(nb, interface_dict)
+        intf_obj = VirtualInterface(netbox_obj, interface_dict)
         intf_obj.create_or_update()
         ip_address_dict = make_ip_address_dict(ip_addresses_dict, interface_dict)
-        vip_obj = VirtualIpAddress(nb, ip_address_dict)
+        vip_obj = VirtualIpAddress(netbox_obj, ip_address_dict)
         vip_obj.create_or_update()
-        assign_primary_ip_to_vm(
-            interface_dict["ip4"],
-            vm_dict[vm_key]["vm"],
-            interface_dict["interface"],
-        )
         print("---")
 
 
@@ -251,8 +193,9 @@ def get_runner_dict():
     runner[16] = {"key": "virtual_machines", "object": VirtualMachine}
     return runner
 
+
 cfg = get_parser()
-nb = netbox()
+netbox_obj = netbox()
 info = load_yaml(cfg.yaml)
 fix_deprecations()
 runner_dict = get_runner_dict()
@@ -263,20 +206,20 @@ for item in sorted(runner_dict):
         # special case for devices
         devices_create_update(
             info["interfaces"],
-            info['ip4_addresses'],
+            info["ip4_addresses"],
             info[key],
-            runner_dict[item]["object"]
+            runner_dict[item]["object"],
         )
         continue
     if key == "virtual_machines":
         # special case for virtual_machines
         virtual_machines_create_update(
             info["virtual_interfaces"],
-            info['ip4_addresses'],
+            info["ip4_addresses"],
             info[key],
-            runner_dict[item]["object"]
+            runner_dict[item]["object"],
         )
         continue
     for entity in info[key]:
-        obj = runner_dict[item]["object"](nb, info[key][entity])
+        obj = runner_dict[item]["object"](netbox_obj, info[key][entity])
         obj.create_or_update()

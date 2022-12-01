ffefa32a86b0bec6b068d820fe5df2ce70eec6d6
Allen Robel <arobel@cisco.com>
2022-12-01 13:29:26 -1000

    run thru pylint/black

--- scripts/console_ports_print.py
+++ scripts/console_ports_print.py
@@ -1,46 +1,62 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: console_ports_print.py
 Description: Display information about all console_ports
-'''
-our_version = 100
+"""
 import argparse
 from netbox_tools.common import netbox, get_console_ports
 
+OUR_VERSION = 101
+
+
 def get_parser():
+    """
+    return an argparse parser object
+    """
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Display information about all console_ports')
+        description="DESCRIPTION: Display information about all console_ports"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(our_version))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
+
 def print_headers():
-    print(fmt.format('id',    'device', 'port',   'tags',   'description'))
-    print(fmt.format('-' * 4, '-' * 15, '-' * 15, '-' * 15, '-'* 15))
+    """
+    print table headers
+    """
+    print(FMT.format("id", "device", "port", "description", "tags"))
+    print(FMT.format("-" * 4, "-" * 15, "-" * 15, "-" * 20, "-" * 40))
+
+
 def get_tag_names(tags_list):
-    tag_names = list()
+    """
+    return a list of tag names associated with the console port
+    """
+    tag_names = []
     for tag_record in tags_list:
         tag_dict = dict(tag_record)
-        tag_names.append(tag_dict['name'])
+        tag_names.append(tag_dict["name"])
     return tag_names
-        
-fmt = '{:>4} {:<15} {:<15} {:<15} {:<15}'
+
+
+FMT = "{:>4} {:<15} {:<15} {:<20} {:<40}"
 
 cfg = get_parser()
-nb = netbox()
+netbox_obj = netbox()
 
-console_ports = get_console_ports(nb)
+console_ports = get_console_ports(netbox_obj)
 print_headers()
 for console_port in console_ports:
-    print(fmt.format(
-        console_port.id,
-        console_port.device.name,
-        console_port.name,
-        ','.join(get_tag_names(console_port.tags)),
-        console_port.description))
+    print(
+        FMT.format(
+            console_port.id,
+            console_port.device.name,
+            console_port.name,
+            console_port.description,
+            ",".join(get_tag_names(console_port.tags)),
+        )
+    )

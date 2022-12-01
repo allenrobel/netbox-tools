576b7f24db0b1de5602a0508cbaadee7580305cc
Allen Robel <arobel@cisco.com>
2022-12-01 12:53:54 -1000

    run thru pylint/black

--- scripts/cluster_create_update_all.py
+++ scripts/cluster_create_update_all.py
@@ -1,39 +1,44 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: cluster_create_update_all.py
 Description: Create/update clusters defined in ``--yaml`
-'''
-our_version = 100
+"""
 import argparse
 from netbox_tools.common import netbox, load_yaml
 from netbox_tools.cluster import Cluster
 
-def get_parser():
-    help_yaml = 'YAML file containing cluster type information.'
+OUR_VERSION = 101
 
-    ex_prefix     = 'Example: '
-    ex_yaml = '{} --yaml ./clusters.yml'.format(ex_prefix)
+
+def get_parser():
+    """
+    return an argparse parser object
+    """
+    help_yaml = "YAML file containing cluster type information."
+
+    ex_prefix = "Example: "
+    ex_yaml = f"{ex_prefix} --yaml ./clusters.yml"
 
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Create/update Netbox clusters defined in ``--yaml``')
+        description="DESCRIPTION: Create/update Netbox clusters defined in ``--yaml``"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
 
-    mandatory.add_argument('--yaml',
-                        dest='yaml',
-                        required=True,
-                        help=help_yaml + ex_yaml)
+    mandatory.add_argument(
+        "--yaml", dest="yaml", required=True, help=help_yaml + ex_yaml
+    )
 
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(our_version))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
+
 cfg = get_parser()
-nb = netbox()
+netbox_obj = netbox()
 info = load_yaml(cfg.yaml)
-for key in info['clusters']:
-    c = Cluster(nb, info['clusters'][key])
+for key in info["clusters"]:
+    c = Cluster(netbox_obj, info["clusters"][key])
     c.create_or_update()

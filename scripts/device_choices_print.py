4980793f5b9a3b00dca162e179dc6edd2981f152
Allen Robel <arobel@cisco.com>
2022-12-02 10:44:43 -1000

    run thru pylint/black, more...
    Add version info

--- scripts/device_choices_print.py
+++ scripts/device_choices_print.py
@@ -1,28 +1,53 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: device_choices_print.py
 Description: Display choices associated with Netbox endpoint dcim.devices
-'''
+"""
+import argparse
 from netbox_tools.common import netbox
 
+OUR_VERSION = 103
+
+
+def get_parser():
+    """
+    return an argparse parser object
+    """
+    parser = argparse.ArgumentParser(
+        description="DESCRIPTION: Display choices associated with Netbox endpoint dcim.devices"
+    )
+
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s v{OUR_VERSION}"
+    )
+
+    return parser.parse_args()
+
+
 def print_header():
-    print(fmt.format(
-        'key',
-        'valid_values'))
-    print(fmt.format(
-        '-' * 15,
-        '-' * 65))
-def print_choices(items):
+    """
+    print table header
+    """
+    print(FMT.format("key", "valid_values"))
+    print(FMT.format("-" * 15, "-" * 65))
+
+
+def print_choices():
+    """
+    print table data
+    """
     for item in items:
-        choices = list()
-        for d in items[item]:
-            choices.append(d['value'])
-        print(fmt.format(item, ', '.join(choices)))
+        choices = []
+        for item_dict in items[item]:
+            choices.append(item_dict["value"])
+        print(FMT.format(item, ", ".join(choices)))
 
-nb = netbox()
 
-items = nb.dcim.devices.choices()
+netbox_obj = netbox()
+cfg = get_parser()
 
-fmt = '{:<15} {:<65}'
+items = netbox_obj.dcim.devices.choices()
+
+FMT = "{:<15} {:<65}"
 print_header()
-print_choices(items)
+print_choices()

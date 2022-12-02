ffb9a6f0a7e56295a8f887113ab3dd2270f001ff
Allen Robel <arobel@cisco.com>
2022-12-02 07:58:02 -1000

    run thru pylint/black

--- scripts/console_server_ports_print.py
+++ scripts/console_server_ports_print.py
@@ -1,46 +1,62 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: console_server_ports_print.py
 Description: Display information about all console_server_ports
-'''
-our_version = 102
+"""
 import argparse
 from netbox_tools.common import netbox, get_console_server_ports
 
+OUR_VERSION = 102
+
+
 def get_parser():
+    """
+    return an argparse parser object
+    """
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Display information about all console_server_ports')
+        description="DESCRIPTION: Display information about all console_server_ports"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(our_version))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
+
 def print_headers():
-    print(fmt.format('id',    'device', 'port',   'tags',   'description'))
-    print(fmt.format('-' * 4, '-' * 15, '-' * 15, '-' * 15, '-'* 15))
+    """
+    print table headers
+    """
+    print(FMT.format("id", "device", "port", "description", "tags"))
+    print(FMT.format("-" * 4, "-" * 15, "-" * 6, "-" * 30, "-" * 40))
+
+
 def get_tag_names(tags_list):
-    tag_names = list()
+    """
+    return a list of tag names associated with the console server port
+    """
+    tag_names = []
     for tag_record in tags_list:
         tag_dict = dict(tag_record)
-        tag_names.append(tag_dict['name'])
+        tag_names.append(tag_dict["name"])
     return tag_names
-        
-fmt = '{:>4} {:<15} {:<15} {:<15} {:<15}'
+
+
+FMT = "{:>4} {:<15} {:<6} {:<30} {:<40}"
 
 cfg = get_parser()
-nb = netbox()
+netbox_obj = netbox()
 
-console_server_ports = get_console_server_ports(nb)
+console_server_ports = get_console_server_ports(netbox_obj)
 print_headers()
 for console_server_port in console_server_ports:
-    print(fmt.format(
-        console_server_port.id,
-        console_server_port.device.name,
-        console_server_port.name,
-        ','.join(get_tag_names(console_server_port.tags)),
-        console_server_port.description))
+    print(
+        FMT.format(
+            console_server_port.id,
+            console_server_port.device.name,
+            console_server_port.name,
+            console_server_port.description,
+            ",".join(get_tag_names(console_server_port.tags)),
+        )
+    )

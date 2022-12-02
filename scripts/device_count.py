f8a2f203c79efc8a7adbac3a82644e0d5a401729
Allen Robel <arobel@cisco.com>
2022-12-02 11:38:18 -1000

    fixed role queries, and run thru pylint/black

--- scripts/device_count.py
+++ scripts/device_count.py
@@ -1,135 +1,211 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: device_count.py
 Description: Print the number of devices matching a given query
 
-Multiple query parameters can be combined to and are boolean ANDed together for
-greater specificity.
+Multiple query parameters can be combined and are boolean ANDed together.
 
 For example:
 
-(py310) netbox-tools % ./device_count.py --site SJC03-1-155 --role leaf --model N9K-C93108TC-FX
-Vault password: 
-"Query: {'role': 'leaf', 'site': 'SJC03-1-155', 'model': 'N9K-C93108TC-FX'}"
+(py310) netbox-tools % ./device_count.py --site S03-1-155 --role leaf --model N9K-C93108TC-FX
+Vault password:
+"Query: {'role': 'leaf', 'site': 'S03-1-155', 'model': 'N9K-C93108TC-FX'}"
 Count: 1
-(py310) netbox-tools % ./device_count.py --site SJC03-1-155 --role leaf                        
-Vault password: 
-"Query: {'role': 'leaf', 'site': 'SJC03-1-155'}"
+(py310) netbox-tools % ./device_count.py --site S03-1-155 --role leaf
+Vault password:
+"Query: {'role': 'leaf', 'site': 'S03-1-155'}"
 Count: 10
-(py310) netbox-tools % 
+(py310) netbox-tools %
 
-'''
-our_version = 103
+"""
 import argparse
+from inspect import stack
+import sys
 import pprint
-
 from netbox_tools.common import netbox
 
+OUR_VERSION = 104
+
+
 def get_parser():
-    help_role = 'Filter on role.'
-    help_site = 'Filter on site.'
-    help_model = 'Filter on model number (e.g. N9K-C93180YC-EX).'
+    """
+    return an argparse parser object
+    """
+    help_role = "Filter on role."
+    help_site = "Filter on site."
+    help_model = "Filter on model number (e.g. N9K-C93180YC-EX)."
 
-    ex_prefix     = 'Example: '
-    ex_role = '{} --role leaf'.format(ex_prefix)
-    ex_site = '{} --site f1'.format(ex_prefix)
-    ex_model = '{} --type N9K-C93180YC-EX'.format(ex_prefix)
+    ex_prefix = "Example: "
+    ex_role = f"{ex_prefix} --role leaf"
+    ex_site = f"{ex_prefix} --site f1"
+    ex_model = f"{ex_prefix} --type N9K-C93180YC-EX"
 
-    parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Netbox: Count devices matching filters [role, site, model].  If no filter is provided, all devices are counted.  Filters are boolean ANDed together.')
+    description = (
+        "DESCRIPTION: Netbox: Count devices matching filters [role, site, model]."
+    )
+    description += " If no filter is provided, all devices are counted."
+    description += " Filters are boolean ANDed together."
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+    parser = argparse.ArgumentParser(description=description)
 
-    default.add_argument('--role',
-                        dest='role',
-                        required=False,
-                        default=None,
-                        help=help_role + ex_role)
-    default.add_argument('--site',
-                        dest='site',
-                        required=False,
-                        default=None,
-                        help=help_site + ex_site)
-    default.add_argument('--model',
-                        dest='model',
-                        required=False,
-                        default=None,
-                        help=help_model + ex_model)
+    default = parser.add_argument_group(title="DEFAULT SCRIPT ARGS")
 
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(our_version))
+    default.add_argument(
+        "--role",
+        dest="role",
+        required=False,
+        default=None,
+        help=f"{help_role} {ex_role}",
+    )
+    default.add_argument(
+        "--site",
+        dest="site",
+        required=False,
+        default=None,
+        help=f"{help_site} {ex_site}",
+    )
+    default.add_argument(
+        "--model",
+        dest="model",
+        required=False,
+        default=None,
+        help=f"{help_model} {ex_model}",
+    )
+
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
+
+def log(*args):
+    """
+    simple logger
+    """
+    print(f"{stack()[1].function}(v{OUR_VERSION}): {' '.join(args)}")
+
+
 def get_args():
-    args = dict()
-    if cfg.role != None:
-        args['role'] = cfg.role
-    if cfg.site != None:
-        args['site'] = cfg.site
-    if cfg.model != None:
-        args['model'] = cfg.model
+    """
+    return a dictionary populated with the user's arguments
+    key is the argument name
+    value is the argument value
+    """
+    args = {}
+    if cfg.role is not None:
+        args["role"] = cfg.role
+    if cfg.site is not None:
+        args["site"] = cfg.site
+    if cfg.model is not None:
+        args["model"] = cfg.model
     return args
 
+
 def verify_model():
-    # 'device_type' does not work with count() for some reason.  We're using device_type.id as a workaround for now.
-    models = dict()
-    if cfg.model == None:
+    """
+    return a dictionary of existing netbox device types.
+    key is device type name, value is netbox device type id.
+    dictionary will be empty if the user didn't specify --model
+
+    Exit with error if the device type does not exist in netbox.
+
+    NOTES:
+    1. 'device_type' does not work with count() for some reason.
+       We're using device_type.id as a workaround for now.
+    """
+    models = {}
+    if cfg.model is None:
         return models
-    items = nb.dcim.device_types.all()
+    items = netbox_obj.dcim.device_types.all()
     for item in items:
         models[item.model] = item.id
-    if cfg.model in models.keys():
+    if cfg.model in models:
         return models
-    else:
-        print('Exiting. Model {} is not present in Netbox. Valid models: {}'.format(cfg.model, ', '.join(sorted(models))))
-        exit(1)
+    log(
+        f"exiting. Model {cfg.model} is not present in Netbox.",
+        f"Valid models: {', '.join(sorted(models))}",
+    )
+    sys.exit(1)
+
+
 def verify_role():
-    roles = dict()
-    if cfg.role == None:
+    """
+    return a dictionary of existing netbox device roles.
+    key is device role name, value is netbox device role id.
+    dictionary will be empty if the user didn't specify --role
+
+    Exit with error if the device role does not exist in netbox.
+
+    NOTES:
+    1. 'role' does not work with count() for some reason.
+       We're using role.id as a workaround for now.
+    """
+    roles = {}
+    if cfg.role is None:
         return roles
-    items = nb.dcim.device_roles.all()
+    items = netbox_obj.dcim.device_roles.all()
     for item in items:
-        roles[item.name] = item.name
-    if cfg.role in roles.keys():
+        roles[item.name] = item.id
+    if cfg.role in roles:
         return roles
-    else:
-        print('Exiting. Role {} is not present in Netbox. Valid roles: {}'.format(cfg.role, ', '.join(sorted(roles.keys()))))
-        exit(1)
+    log(
+        f"exiting. Role {cfg.role} is not present in Netbox.",
+        f"Valid roles: {', '.join(sorted(roles))}",
+    )
+    sys.exit(1)
+
+
 def verify_site():
-    # 'site' does not work with count() for some reason.  We're using site.id as a workaround for now.
-    sites = dict()
-    if cfg.site == None:
+    """
+    return a dictionary of existing netbox sites.
+    key is site name, value is netbox site id.
+    dictionary will be empty if the user didn't specify --site
+
+    Exit with error if the device type does not exist in netbox.
+
+    NOTES:
+    1. 'site' does not work with count() for some reason.
+       We're using site.id as a workaround for now.
+    """
+    sites = {}
+    if cfg.site is None:
         return sites
-    items = nb.dcim.sites.all()
+    items = netbox_obj.dcim.sites.all()
     for item in items:
         sites[item.name] = item.id
-    if cfg.site in sites.keys():
+    if cfg.site in sites:
         return sites
-    else:
-        print('Exiting. Site {} is not present in Netbox. Valid sites: {}'.format(cfg.site, ', '.join(sorted(sites.keys()))))
-        exit(1)
+    log(
+        f"exiting. Site {cfg.site} is not present in Netbox.",
+        f"Valid sites: {', '.join(sorted(sites))}",
+    )
+    sys.exit(1)
+
 
 def get_device_count():
+    """
+    return the number of devices matching a given query
+    """
     models = verify_model()
-    roles = verify_role()
+    role_ids = verify_role()
     site_ids = verify_site()
-    args = dict()
+    args = {}
     try:
         if len(site_ids) != 0:
-            args['site_id'] = site_ids[cfg.site]
-        if len(roles) != 0:
-            args['role'] = roles[cfg.role]
+            args["site_id"] = site_ids[cfg.site]
+        if len(role_ids) != 0:
+            args["role_id"] = role_ids[cfg.role]
         if len(models.keys()) != 0:
-            args['device_type_id'] = models[cfg.model]
-        return nb.dcim.devices.count(**args)
-    except Exception as e:
-        print('Unable to get count: {}'.format(e))
+            args["device_type_id"] = models[cfg.model]
+        return netbox_obj.dcim.devices.count(**args)
+    except Exception as general_exception:
+        log("Unable to get count.", f"Exception detail: {general_exception}")
+        sys.exit(1)
+
 
 cfg = get_parser()
-nb = netbox()
+netbox_obj = netbox()
 count = get_device_count()
-pprint.pprint('Query: {}'.format(get_args()))
-print('Count: {}'.format(count))
+pprint.pprint(f"Query: {get_args()}")
+print(f"Count: {count}")

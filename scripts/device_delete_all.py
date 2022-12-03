9ced64c2eda7ff0f772a625436afd153fc2256e2
Allen Robel <arobel@cisco.com>
2022-12-02 17:47:02 -1000

    run thru pylint/black

--- scripts/device_delete_all.py
+++ scripts/device_delete_all.py
@@ -1,45 +1,50 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: device_create_all.py
 Summary: Delete all devices contained in the YAML file pointed to with --yaml
 Description: Delete all devices in YAML file
 
-Be careful!  This will not ask for confirmation. 
+Be careful!  This will not ask for confirmation.
 
-'''
-our_version = 102
+"""
 import argparse
 from netbox_tools.common import netbox
 from netbox_tools.common import load_yaml
 from netbox_tools.device import Device
 
-def get_parser():
-    help_yaml = 'Name of yaml file containing devices to delete.'
+OUR_VERSION = 103
 
-    ex_prefix = ' Example: '
-    ex_yaml = '{} --yaml /path/to/my.yml'.format(ex_prefix)
+
+def get_parser():
+    """
+    return an argparse parser object
+    """
+    help_yaml = "Name of yaml file containing devices to delete."
+
+    ex_prefix = " Example: "
+    ex_yaml = f"{ex_prefix} --yaml /path/to/my.yml"
 
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Netbox: Delete all devices in YAML file')
+        description="DESCRIPTION: Netbox: Delete all devices in YAML file"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
 
-    mandatory.add_argument('--yaml',
-                        dest='yaml',
-                        required=True,
-                        help=help_yaml + ex_yaml)
+    mandatory.add_argument(
+        "--yaml", dest="yaml", required=True, help=f"{help_yaml} {ex_yaml}"
+    )
 
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(our_version))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
+
 cfg = get_parser()
-nb = netbox()
+netbox_obj = netbox()
 
 info = load_yaml(cfg.yaml)
-for key in info['devices']:
-    d = Device(nb, info['devices'][key])
-    d.delete()
+for key in info["devices"]:
+    device_obj = Device(netbox_obj, info["devices"][key])
+    device_obj.delete()

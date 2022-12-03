c9f2dcfe84f9e66fd75426ddec92088aa855ec04
Allen Robel <arobel@cisco.com>
2022-12-02 21:04:22 -1000

    run thru black/pylint

--- scripts/device_delete.py
+++ scripts/device_delete.py
@@ -1,42 +1,48 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: device_create_all.py
 Summary: Delete all devices contained in the YAML file pointed to with --yaml
 Description: Delete device --device from netbox
-'''
-our_version = 103
+"""
 import argparse
 from netbox_tools.common import netbox
 from netbox_tools.device import Device
 
+OUR_VERSION = 104
+
+
 def get_parser():
-    help_device = 'Name of the device to delete.'
+    """
+    return an argparse parser object
+    """
+    help_device = "Name of the device to delete."
 
-    ex_prefix = ' Example: '
-    ex_device = '{} --device leaf_3'.format(ex_prefix)
+    ex_prefix = " Example: "
+    ex_device = f"{ex_prefix} --device leaf_3"
 
-    parser = argparse.ArgumentParser(description='DESCRIPTION: Netbox: Delete a device')
+    parser = argparse.ArgumentParser(description="DESCRIPTION: Netbox: Delete a device")
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
 
-    mandatory.add_argument('--device',
-                        dest='device',
-                        required=True,
-                        help=help_device + ex_device)
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(our_version))
-
+    mandatory.add_argument(
+        "--device", dest="device", required=True, help=f"{help_device} {ex_device}"
+    )
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
     return parser.parse_args()
 
+
 def get_info():
-    info = dict()
-    info['device'] = cfg.device
+    """
+    return delete operation parameters expected by Device()
+    """
+    info = {}
+    info["device"] = cfg.device
     return info
 
+
 cfg = get_parser()
-nb = netbox()
-d = Device(nb, get_info())
-d.delete()
+netbox_obj = netbox()
+device_obj = Device(netbox_obj, get_info())
+device_obj.delete()

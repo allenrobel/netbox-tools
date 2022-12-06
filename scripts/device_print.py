950bfb1184437cdaec74a4823213c8da29f19674
Allen Robel <arobel@cisco.com>
2022-12-06 13:42:16 -1000

    run thru pylint and improve logging

--- scripts/device_print.py
+++ scripts/device_print.py
@@ -1,67 +1,93 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: device_print.py
 Description: Print information about a device
-'''
-our_version = 102
+"""
 import argparse
+from inspect import stack
+import sys
 import json
 from netbox_tools.common import netbox
 
-def get_parser():
-    help_name = 'Name of device to retrieve.'
-    help_detail = 'Optional. If present, print detailed info about device.'
+OUR_VERSION = 103
 
-    ex_prefix     = 'Example: '
-    ex_name = '{} --name leaf_3'.format(ex_prefix)
-    ex_detail = '{} --detail'.format(ex_prefix)
+
+def get_parser():
+    """
+    return an argparse parser object
+    """
+    help_name = "Name of device to retrieve."
+    help_detail = "Optional. If present, print detailed info about device."
+
+    ex_prefix = "Example: "
+    ex_name = f"{ex_prefix} --name leaf_3"
+    ex_detail = f"{ex_prefix} --detail"
 
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Print information about a device')
+        description="DESCRIPTION: Print information about a device"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
+    default = parser.add_argument_group(title="DEFAULT SCRIPT ARGS")
 
-    mandatory.add_argument('--name',
-                        dest='name',
-                        required=True,
-                        help=help_name + ex_name)
-    default.add_argument('--detail',
-                        dest='detail',
-                        required=False,
-                        default=False,
-                        action='store_true',
-                        help=help_detail + ex_detail)
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(our_version))
-
+    mandatory.add_argument(
+        "--name", dest="name", required=True, help=f"{help_name} {ex_name}"
+    )
+    default.add_argument(
+        "--detail",
+        dest="detail",
+        required=False,
+        default=False,
+        action="store_true",
+        help=f"{help_detail} {ex_detail}",
+    )
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
     return parser.parse_args()
 
 
+def log(*args):
+    """
+    simple logger
+    """
+    print(f"{stack()[1].function}(v{OUR_VERSION}): {' '.join(args)}")
+
+
 def get_device():
-    device = nb.dcim.devices.get(name=cfg.name)
-    if device == None:
-        print('Device {} does not exist in netbox.'.format(cfg.name))
-        exit(1)
+    """
+    return device object associated with --name
+    """
+    device = netbox_obj.dcim.devices.get(name=cfg.name)
+    if device is None:
+        log(f"Device {cfg.name} does not exist at {netbox_obj.base_url}.")
+        sys.exit(1)
     return device
 
+
 def print_detail():
-    pretty = json.dumps(dict(device), indent=4, sort_keys=True)
+    """
+    print raw json returned by Netbox for device_obj
+    """
+    pretty = json.dumps(dict(device_obj), indent=4, sort_keys=True)
     print(pretty)
 
-def print_headers():
-    print(fmt.format('id', 'device_name', 'comments'))
-    print(fmt.format('-' * 5, '-' * 20, '-' * 30))
 
-fmt = '{:>5} {:<20} {:<30}'
+def print_headers():
+    """
+    print table headers
+    """
+    print(FMT.format("id", "device_name", "comments"))
+    print(FMT.format("-" * 5, "-" * 20, "-" * 30))
+
+
+FMT = "{:>5} {:<20} {:<30}"
 
 cfg = get_parser()
-nb = netbox()
-device = get_device()
+netbox_obj = netbox()
+device_obj = get_device()
 if cfg.detail:
     print_detail()
 else:
     print_headers()
-    print(fmt.format(device.id, device.name, device.comments))
+    print(FMT.format(device_obj.id, device_obj.name, device_obj.comments))

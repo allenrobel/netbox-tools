8b8543bfb0deaeedc207e6cd202b5229ba9da480
Allen Robel <arobel@cisco.com>
2022-12-07 13:29:30 -1000

    run thru pylint/black

--- scripts/interface_create_update_all.py
+++ scripts/interface_create_update_all.py
@@ -1,41 +1,45 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: interface_create_update_all.py
 Description: Create/update all ip prefixes defined in ``--yaml``
-'''
-our_version = 101
+"""
 import argparse
-
 from netbox_tools.common import netbox, load_yaml
 from netbox_tools.interface import Interface
 
-def get_parser():
-    help_yaml = 'YAML file in which prefix information can be found.'
+OUR_VERSION = 102
 
-    ex_prefix_ = ' Example: '
-    ex_yaml = '{} --yaml ./info.yml'.format(ex_prefix_)
+
+def get_parser():
+    """
+    return an argparse parser object
+    """
+    help_yaml = "YAML file in which prefix information can be found."
+
+    ex_prefix_ = " Example: "
+    ex_yaml = f"{ex_prefix_} --yaml ./info.yml"
 
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Netbox: Create or update all interfaces')
+        description="DESCRIPTION: Netbox: Create or update all interfaces"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
 
-    mandatory.add_argument('--yaml',
-                        dest='yaml',
-                        required=True,
-                        help=help_yaml + ex_yaml)
+    mandatory.add_argument(
+        "--yaml", dest="yaml", required=True, help=f"{help_yaml} {ex_yaml}"
+    )
 
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(our_version))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
+
 cfg = get_parser()
 info = load_yaml(cfg.yaml)
-nb = netbox()
-print('---')
-for key in info['interfaces']:
-    i = Interface(nb, info['interfaces'][key])
+netbox_obj = netbox()
+print("---")
+for key in info["interfaces"]:
+    i = Interface(netbox_obj, info["interfaces"][key])
     i.create_or_update()

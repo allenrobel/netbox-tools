dcbe2a62755e74727db41cbce17b87810694056f
Allen Robel <arobel@cisco.com>
2022-12-07 11:35:29 -1000

    run thru pylint/black

--- scripts/device_types_print.py
+++ scripts/device_types_print.py
@@ -1,41 +1,74 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: device_types_print.py
 Description: Display summary information about all device types
-'''
-our_version = 103
+"""
 import argparse
+from inspect import stack
+import sys
 from netbox_tools.common import netbox
 
+OUR_VERSION = 104
+
+
 def get_parser():
+    """
+    return an argparse parser object
+    """
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Print summary information about all device types')
+        description="DESCRIPTION: Print summary information about all device types"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(our_version))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
+
+def log(*args):
+    """
+    simple logger
+    """
+    print(f"{stack()[1].function}(v{OUR_VERSION}): {' '.join(args)}")
+
+
+
 def get_device_types():
+    """
+    return all Netbox device types
+    """
     try:
-        device_types = nb.dcim.device_types.all()
-        return device_types
-    except Exception as e:
-        print('Could not retrieve device types. Error was {}'.format(e))
+        result = nb.dcim.device_types.all()
+        return result
+    except Exception as _general_exception:
+        log(
+            "Could not retrieve device types.",
+            f"Exception detail: {_general_exception}"
+        )
+        sys.exit(1)
+
 
 def print_headers():
-    print(fmt.format('id', 'name', 'device_count', 'manufacturer'))
-    print(fmt.format('-' * 5, '-' * 15, '-' * 12, '-' * 20))
+    """
+    print table headers
+    """
+    print(FMT.format("id", "name", "device_count", "manufacturer"))
+    print(FMT.format("-" * 5, "-" * 15, "-" * 12, "-" * 20))
+
 
 cfg = get_parser()
 nb = netbox()
 
 device_types = get_device_types()
-fmt = '{:>5} {:<15} {:>12} {:<20}'
+FMT = "{:>5} {:<15} {:>12} {:<20}"
 print_headers()
-for dt in device_types:
-    print(fmt.format(dt.id, dt.model, dt.device_count, dt.manufacturer.name))
+for device_type_obj in device_types:
+    print(
+        FMT.format(
+            device_type_obj.id,
+            device_type_obj.model,
+            device_type_obj.device_count,
+            device_type_obj.manufacturer.name
+        )
+    )

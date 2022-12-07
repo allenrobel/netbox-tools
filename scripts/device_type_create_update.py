e18f1303e69ec73e456cf05327c83884ef8c5fec
Allen Robel <arobel@cisco.com>
2022-12-06 14:12:46 -1000

    run thru pylint/black, more...
    remove unused imports, re and load_yaml

--- scripts/device_type_create_update.py
+++ scripts/device_type_create_update.py
@@ -1,69 +1,80 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: device_type_create_update.py
 Description: Create or update a Netbox device type using command line options
-'''
-our_version = 104
+"""
 import argparse
-import re
-from netbox_tools.common import netbox, load_yaml
+from netbox_tools.common import netbox
 from netbox_tools.device_type import DeviceType
 
+OUR_VERSION = 105
+
+
 def get_parser():
-    help_comments = 'Freeform comments for this device type.'
-    help_manufacturer = 'Who makes this device type'
-    help_model = 'Typically, the product/model number for this device type'
-    help_tags = 'A comma-separated list of tags for this device type (these must all exist in netbox already)'
-    ex_prefix     = 'Example: '
-    ex_comments = '{} --comments "36x40/100G QSFP28 Ethernet Module"'.format(ex_prefix)
-    ex_manufacturer = '{} --manufacturer cisco'.format(ex_prefix)
-    ex_model = '{} --model N9K-C9336C-FX2'.format(ex_prefix)
-    ex_tags = '{} --tags admin,infra'
+    """
+    return an argparse parser object
+    """
+    help_comments = "Freeform comments for this device type"
+    help_manufacturer = "Who makes this device type"
+    help_model = "Typically, the product/model number for this device type"
+    help_tags = "A comma-separated list of tags for this device type"
+    help_tags += " (these must already exist in netbox)"
+    ex_prefix = "Example: "
+    ex_comments = f"{ex_prefix} --comments '36x40/100G QSFP28 Ethernet Module'"
+    ex_manufacturer = f"{ex_prefix} --manufacturer cisco"
+    ex_model = f"{ex_prefix} --model N9K-C9336C-FX2"
+    ex_tags = f"{ex_prefix} --tags admin,infra"
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Create or update a Netbox device type (Netbox device type is roughly equivilent to model number)')
+        description="DESCRIPTION: Create or update a Netbox device type"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    optional   = parser.add_argument_group(title='OPTIONAL SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
+    optional = parser.add_argument_group(title="OPTIONAL SCRIPT ARGS")
 
-    optional.add_argument('--comments',
-                        dest='comments',
-                        required=False,
-                        default=None,
-                        help=help_comments + ex_comments)
+    optional.add_argument(
+        "--comments",
+        dest="comments",
+        required=False,
+        default=None,
+        help=f"{help_comments} {ex_comments}",
+    )
 
-    mandatory.add_argument('--manufacturer',
-                        dest='manufacturer',
-                        required=True,
-                        help=help_manufacturer + ex_manufacturer)
+    mandatory.add_argument(
+        "--manufacturer",
+        dest="manufacturer",
+        required=True,
+        help=f"{help_manufacturer} {ex_manufacturer}",
+    )
 
-    mandatory.add_argument('--model',
-                        dest='model',
-                        required=True,
-                        help=help_model + ex_model)
+    mandatory.add_argument(
+        "--model", dest="model", required=True, help=f"{help_model} {ex_model}"
+    )
 
-    optional.add_argument('--tags',
-                        dest='tags',
-                        required=False,
-                        default=None,
-                        help=help_tags + ex_tags)
+    optional.add_argument(
+        "--tags",
+        dest="tags",
+        required=False,
+        default=None,
+        help=f"{help_tags} {ex_tags}",
+    )
 
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(our_version))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s v{OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
+
 cfg = get_parser()
-nb = netbox()
+netbox_obj = netbox()
 
-info = dict()
-info['manufacturer'] = cfg.manufacturer
-info['model'] = cfg.model
-if cfg.comments != None:
-    info['comments'] = cfg.comments
-if cfg.tags != None:
-    info['tags'] = re.split(',', cfg.tags)
+info = {}
+info["manufacturer"] = cfg.manufacturer
+info["model"] = cfg.model
+if cfg.comments is not None:
+    info["comments"] = cfg.comments
+if cfg.tags is not None:
+    info["tags"] = cfg.tags.split(",")
 
-d = DeviceType(nb, info)
+d = DeviceType(netbox_obj, info)
 d.create_or_update()
-

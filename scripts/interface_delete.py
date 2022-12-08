a54a519648dfada4634026453cff3834640c85b1
Allen Robel <arobel@cisco.com>
2022-12-07 14:15:17 -1000

    run thru pylint/black

--- scripts/interface_delete.py
+++ scripts/interface_delete.py
@@ -1,49 +1,60 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: interface_delete.py
 Description: Delete interface ``--interface`` from netbox
-'''
-our_version = 102
+"""
 import argparse
-
 from netbox_tools.common import netbox
 from netbox_tools.interface import Interface
 
+OUR_VERSION = 103
+
+
 def get_parser():
-    help_device = 'Name of the device associated with --interface'
-    help_interface = 'Name of the interface to delete.'
+    """
+    return an argparse parser object
+    """
+    help_device = "Name of the device associated with --interface"
+    help_interface = "Name of the interface to delete."
 
-    ex_prefix = ' Example: '
-    ex_device = '{} --device leaf_3'.format(ex_prefix)
-    ex_interface = '{} --interface mgmt0'.format(ex_prefix)
+    ex_prefix = " Example: "
+    ex_device = f"{ex_prefix} --device leaf_3"
+    ex_interface = f"{ex_prefix} --interface mgmt0"
 
-    parser = argparse.ArgumentParser(description='DESCRIPTION: Delete interface --interface from netbox')
+    parser = argparse.ArgumentParser(
+        description="DESCRIPTION: Delete interface --interface from netbox"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
 
-    mandatory.add_argument('--device',
-                        dest='device',
-                        required=True,
-                        help=help_device + ex_device)
-    mandatory.add_argument('--interface',
-                        dest='interface',
-                        required=True,
-                        help=help_interface + ex_interface)
+    mandatory.add_argument(
+        "--device", dest="device", required=True, help=f"{help_device} {ex_device}"
+    )
+    mandatory.add_argument(
+        "--interface",
+        dest="interface",
+        required=True,
+        help=f"{help_interface} {ex_interface}"
+    )
 
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(our_version))
+    parser.add_argument(
+        "--version", action="version", version="%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
+
 def get_info():
-    info = dict()
-    info['interface'] = cfg.interface
-    info['device'] = cfg.device
+    """
+    return dictionary expected by Interface().delete()
+    """
+    info = {}
+    info["interface"] = cfg.interface
+    info["device"] = cfg.device
     return info
 
+
 cfg = get_parser()
-nb = netbox()
-i = Interface(nb, get_info())
-i.delete()
+netbox_obj = netbox()
+interface_obj = Interface(netbox_obj, get_info())
+interface_obj.delete()

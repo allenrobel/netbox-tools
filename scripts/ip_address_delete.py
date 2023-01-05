d9f8e9c123de95733a5c9be16dd94d163c87af83
Allen Robel <arobel@cisco.com>
2023-01-05 11:13:35 -1000

    run thru pylint/black

--- scripts/ip_address_delete.py
+++ scripts/ip_address_delete.py
@@ -1,40 +1,44 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: ip_address_delete.py
 Description: Delete ip address ``--ip`` from netbox
-'''
-OUR_VERSION = 100
+"""
 import argparse
 
 from netbox_tools.common import netbox
 from netbox_tools.ip_address import IpAddress
 
+OUR_VERSION = 101
+
+
 def get_parser():
-    help_ip = 'IP address to delete. Specify with format A.B.C.D/E'
+    """
+    return an argparse parser object
+    """
+    help_ip = "IP address to delete. Specify with format A.B.C.D/E"
+    ex_prefix = " Example: "
+    ex_ip = f"{ex_prefix} --ip 192.168.0.0/24"
 
-    ex_prefix = ' Example: '
-    ex_ip = '{} --ip 192.168.0.0/24'.format(ex_prefix)
+    parser = argparse.ArgumentParser(description="DESCRIPTION: Delete ip address")
 
-    parser = argparse.ArgumentParser(description='DESCRIPTION: Delete ip address')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
+    mandatory.add_argument("--ip", dest="ip", required=True, help=help_ip + ex_ip)
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
-
-    mandatory.add_argument('--ip',
-                        dest='ip',
-                        required=True,
-                        help=help_ip + ex_ip)
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
+
 def get_info():
-    info = dict()
-    info['ip4'] = cfg.ip
+    """
+    return dictionary containing arguments expected by IpAddress
+    """
+    info = {}
+    info["ip4"] = cfg.ip
     return info
+
 
 cfg = get_parser()
 nb = netbox()

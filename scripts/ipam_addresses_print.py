0cd34d78b9698031f2713ea925f06f9fc0bc910f
Allen Robel <arobel@cisco.com>
2023-01-05 13:59:06 -1000

    Support virtual_machine type, more...
    Also, run thru pylint/black

--- scripts/ipam_addresses_print.py
+++ scripts/ipam_addresses_print.py
@@ -1,30 +1,69 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: ipam_addresses_print.py
 Description: Display all ip addresses
-'''
+"""
+import sys
 from netbox_tools.common import netbox
 
+
 def get_fmt():
-    return '{:>5} {:<18} {:<18} {:<30}'
+    """
+    return a format string
+    """
+    return "{:>5} {:<18} {:<18} {:>6} {:<30}"
+
 
 def print_ip_addresses():
+    """
+    print column values
+    """
     items = nb.ipam.ip_addresses.all()
-    if items == None:
-        exit()
+    if items is None:
+        sys.exit(0)
     fmt = get_fmt()
     for item in items:
-        if item.assigned_object == None:
-            print(fmt.format(item.id, item.address, 'na', item.description))
+        if "device" in dict(item.assigned_object):
+            print(
+                fmt.format(
+                    item.id,
+                    item.address,
+                    item.assigned_object.device.name,
+                    "device",
+                    item.description,
+                )
+            )
+        elif "virtual_machine" in dict(item.assigned_object):
+            print(
+                fmt.format(
+                    item.id,
+                    item.address,
+                    item.assigned_object.virtual_machine.name,
+                    "vm",
+                    item.description,
+                )
+            )
         else:
-            print(fmt.format(item.id, item.address, item.assigned_object.device.name, item.description))
+            print(
+                fmt.format(
+                    item.id,
+                    item.address,
+                    "na",
+                    "na",
+                    item.description,
+                )
+            )
+
 
 def print_headers():
+    """
+    print column headers
+    """
     fmt = get_fmt()
-    print(fmt.format('id', 'address', 'device_name', 'description'))
-    print(fmt.format('-' * 5, '-' * 18, '-' * 18, '-' * 30))
+    print(fmt.format("id", "address", "name", "type", "description"))
+    print(fmt.format("-" * 5, "-" * 18, "-" * 18, "-" * 6, "-" * 30))
+
 
 nb = netbox()
-
 print_headers()
 print_ip_addresses()

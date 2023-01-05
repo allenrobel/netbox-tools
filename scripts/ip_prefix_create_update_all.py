d477749fd9afd3af7d0fcdc5119c76675f6f6925
Allen Robel <arobel@cisco.com>
2023-01-05 11:27:01 -1000

    run thru pylint/black

--- scripts/ip_prefix_create_update_all.py
+++ scripts/ip_prefix_create_update_all.py
@@ -1,41 +1,45 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: ip_prefix_create_update_all.py
 Description: Create/update all ip prefixes defined in ``--yaml``
-'''
-OUR_VERSION = 103
+"""
 import argparse
-
 from netbox_tools.common import netbox, load_yaml
 from netbox_tools.ip_prefix import IpPrefix
 
-def get_parser():
-    help_yaml = 'YAML file in which prefix information can be found.'
+OUR_VERSION = 104
 
-    ex_prefix_ = ' Example: '
-    ex_yaml = '{} --yaml ./info.yml'.format(ex_prefix_)
+
+def get_parser():
+    """
+    return an argparse parser object
+    """
+    help_yaml = "YAML file in which prefix information can be found."
+
+    ex_prefix = " Example: "
+    ex_yaml = f"{ex_prefix} --yaml ./info.yml"
 
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Netbox: Create or update all ip prefixes')
+        description="DESCRIPTION: Netbox: Create or update all ip prefixes"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
 
-    mandatory.add_argument('--yaml',
-                        dest='yaml',
-                        required=True,
-                        help=help_yaml + ex_yaml)
+    mandatory.add_argument(
+        "--yaml", dest="yaml", required=True, help=help_yaml + ex_yaml
+    )
 
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
+
 
 cfg = get_parser()
 info = load_yaml(cfg.yaml)
 nb = netbox()
-print('---')
-for key in info['prefixes']:
-    p = IpPrefix(nb, info['prefixes'][key])
+print("---")
+for key in info["prefixes"]:
+    p = IpPrefix(nb, info["prefixes"][key])
     p.create_or_update()

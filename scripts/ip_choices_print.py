c9b1e553d519c1c6ab9fa326c71d1055958a1d73
Allen Robel <arobel@cisco.com>
2023-01-05 11:23:36 -1000

    run thru pylint/black

--- scripts/ip_choices_print.py
+++ scripts/ip_choices_print.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: ip_choices_print.py
 Description: Display choices associated with endpoint ipam.ip_addresses
-'''
+"""
 from netbox_tools.common import netbox
 
+
 def print_header():
-    print(fmt.format(
-        'key',
-        'valid_values'))
-    print(fmt.format(
-        '-' * 15,
-        '-' * 65))
+    """
+    print column header labels
+    """
+    print(FMT.format("key", "valid_values"))
+    print(FMT.format("-" * 15, "-" * 65))
+
+
 def print_choices(items):
+    """
+    print column values
+    """
     for item in items:
-        choices = list()
-        for d in items[item]:
-            choices.append(d['value'])
-        print(fmt.format(item, ', '.join(choices)))
+        choices_list = []
+        for item_dict in items[item]:
+            choices_list.append(item_dict["value"])
+        print(FMT.format(item, ", ".join(choices_list)))
+
 
 nb = netbox()
 
-items = nb.ipam.ip_addresses.choices()
+choices = nb.ipam.ip_addresses.choices()
 
-fmt = '{:<15} {:<65}'
+FMT = "{:<15} {:<65}"
 print_header()
-print_choices(items)
+print_choices(choices)

c52f15f2e297ae32acf78023313a6924f6ca1e3a
Allen Robel <arobel@cisco.com>
2023-01-06 10:40:46 -1000

    refactor

--- scripts/ip_choices_print.py
+++ scripts/ip_choices_print.py
@@ -6,29 +6,33 @@ Description: Display choices associated with endpoint ipam.ip_addresses
 from netbox_tools.common import netbox
 
 
+def get_fmt():
+    """
+    return a format string
+    """
+    return "{key:<15} {valid_values:<65}"
+
+
 def print_header():
     """
     print column header labels
     """
-    print(FMT.format("key", "valid_values"))
-    print(FMT.format("-" * 15, "-" * 65))
+    print(get_fmt().format(key="key", valid_values="valid_values"))
+    print(get_fmt().format(key="-" * 15, valid_values="-" * 65))
 
 
-def print_choices(items):
+def print_choices(nb):
     """
     print column values
     """
+    items = nb.ipam.ip_addresses.choices()
     for item in items:
         choices_list = []
         for item_dict in items[item]:
             choices_list.append(item_dict["value"])
-        print(FMT.format(item, ", ".join(choices_list)))
+        print(get_fmt().format(key=item, valid_values=", ".join(choices_list)))
 
 
-nb = netbox()
-
-choices = nb.ipam.ip_addresses.choices()
-
-FMT = "{:<15} {:<65}"
+netbox_obj = netbox()
 print_header()
-print_choices(choices)
+print_choices(netbox_obj)

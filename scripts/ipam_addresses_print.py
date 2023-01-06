ad9ba2d3e7aed21a1f0307354faea66b7b3f907d
Allen Robel <arobel@cisco.com>
2023-01-05 14:17:37 -1000

    tweak format string

--- scripts/ipam_addresses_print.py
+++ scripts/ipam_addresses_print.py
@@ -11,7 +11,7 @@ def get_fmt():
     """
     return a format string
     """
-    return "{:>5} {:<18} {:<18} {:>6} {:<30}"
+    return "{id:>5} {address:<18} {device:<18} {type:>6} {description:<30}"
 
 
 def print_ip_addresses():
@@ -24,35 +24,23 @@ def print_ip_addresses():
     fmt = get_fmt()
     for item in items:
         if "device" in dict(item.assigned_object):
-            print(
-                fmt.format(
-                    item.id,
-                    item.address,
-                    item.assigned_object.device.name,
-                    "device",
-                    item.description,
-                )
-            )
+            name = item.assigned_object.device.name
+            device_type = "device"
         elif "virtual_machine" in dict(item.assigned_object):
-            print(
-                fmt.format(
-                    item.id,
-                    item.address,
-                    item.assigned_object.virtual_machine.name,
-                    "vm",
-                    item.description,
-                )
-            )
+            name = item.assigned_object.virtual_machine.name
+            device_type = "vm"
         else:
-            print(
-                fmt.format(
-                    item.id,
-                    item.address,
-                    "na",
-                    "na",
-                    item.description,
-                )
+            name = "na"
+            device_type = "na"
+        print(
+            fmt.format(
+                id=item.id,
+                address=item.address,
+                device=name,
+                type=device_type,
+                description=item.description,
             )
+        )
 
 
 def print_headers():
@@ -60,8 +48,24 @@ def print_headers():
     print column headers
     """
     fmt = get_fmt()
-    print(fmt.format("id", "address", "name", "type", "description"))
-    print(fmt.format("-" * 5, "-" * 18, "-" * 18, "-" * 6, "-" * 30))
+    print(
+        fmt.format(
+            id="id",
+            address="address",
+            device="name",
+            type="type",
+            description="description",
+        )
+    )
+    print(
+        fmt.format(
+            id="-" * 5,
+            address="-" * 18,
+            device="-" * 18,
+            type="-" * 6,
+            description="-" * 30,
+        )
+    )
 
 
 nb = netbox()

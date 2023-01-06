a45a20a69a714f6ade07b0a9e54fada56eb6c100
Allen Robel <arobel@cisco.com>
2023-01-06 09:45:32 -1000

    run thru pylint/black

--- scripts/site_delete.py
+++ scripts/site_delete.py
@@ -1,40 +1,48 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: site_delete.py
-Description: Delete site ``--site``
-'''
-OUR_VERSION = 103
+Description: Delete Netbox site ``--site``
+"""
 import argparse
-
 from netbox_tools.common import netbox
 from netbox_tools.site import Site
 
+OUR_VERSION = 104
+
+
 def get_parser():
-    help_site = 'Name of the site to delete.'
+    """
+    return an argparse parser object
+    """
+    help_site = "Name of the site to delete."
 
-    ex_prefix = ' Example: '
-    ex_site = '{} --site mysite'.format(ex_prefix)
+    ex_prefix = " Example: "
+    ex_site = f"{ex_prefix} --site mysite"
 
-    parser = argparse.ArgumentParser(description='DESCRIPTION: Netbox: Delete a site')
+    parser = argparse.ArgumentParser(
+        description="DESCRIPTION: Delete Netbox site ``--site`"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
+    mandatory.add_argument(
+        "--site", dest="site", required=True, help=f"{help_site} {ex_site}"
+    )
 
-    mandatory.add_argument('--site',
-                        dest='site',
-                        required=True,
-                        help=help_site + ex_site)
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
+
 def get_info():
-    info = dict()
-    info['name'] = cfg.site
+    """
+    return dictionary containing parameters expected by Site
+    """
+    info = {}
+    info["name"] = cfg.site
     return info
+
 
 cfg = get_parser()
 nb = netbox()

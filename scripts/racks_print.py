d968fb0ed94e5fd05fc343e07cbd2ab7cca9a045
Allen Robel <arobel@cisco.com>
2023-01-05 17:18:19 -1000

    run thru pylint/black

--- scripts/racks_print.py
+++ scripts/racks_print.py
@@ -1,39 +1,50 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: racks_print.py
 Description: Display information about all racks
-'''
-OUR_VERSION = 103
+"""
 import argparse
 from netbox_tools.common import netbox
 
+OUR_VERSION = 104
+
+
 def get_parser():
+    """
+    return an argparse parser object
+    """
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Display information about all racks')
+        description="DESCRIPTION: Display information about all racks"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
+
 def get_racks():
+    """
+    return all rack objects
+    """
     return nb.dcim.racks.all()
 
+
 def print_headers():
-    print(fmt.format('id', 'name', 'site'))
-    print(fmt.format('-' * 5, '-' * 15, '-' * 15))
+    """
+    print column headers
+    """
+    print(FMT.format(id="id", name="name", site="site"))
+    print(FMT.format(id="-" * 5, name="-" * 15, site="-" * 15))
 
 
 cfg = get_parser()
 nb = netbox()
 racks = get_racks()
 
-fmt = '{:>5} {:>15} {:>15}'
+FMT = "{id:>5} {name:>15} {site:>15}"
 
 print_headers()
 for rack in racks:
-    print(fmt.format(rack.id, rack.name, rack.site.name))
+    print(FMT.format(rack.id, rack.name, rack.site.name))

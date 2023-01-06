49a7401e2a80997c0db9d4e9b3109ea0c45b8f35
Allen Robel <arobel@cisco.com>
2023-01-06 11:33:13 -1000

    run thru pylint/black

--- scripts/virtual_ip_address_create_update_all.py
+++ scripts/virtual_ip_address_create_update_all.py
@@ -1,44 +1,46 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: virtual_ip_address_create_update_all.py
-Description: Create/update all virtual_ip_addresses (those belonging to virtual machines) defined in ``--yaml``
-'''
-OUR_VERSION = 100
+Description: Create/update all Netbox virtual_ip_addresses (vm IPs) defined in ``--yaml``
+"""
 import argparse
-
 from netbox_tools.common import netbox, load_yaml, make_ip_address_dict
 from netbox_tools.virtual_ip_address import VirtualIpAddress
 
+OUR_VERSION = 101
+
+
 def get_parser():
-    help_yaml = 'YAML file in which ip_address information can be found.'
+    """
+    return an argparse parser object
+    """
+    help_yaml = "YAML file in which ip_address information can be found."
 
-    ex_prefix_ = ' Example: '
-    ex_yaml = '{} --yaml ./info.yml'.format(ex_prefix_)
+    ex_prefix = " Example: "
+    ex_yaml = f"{ex_prefix} --yaml ./info.yml"
 
-    parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Netbox: Create or update all ip addresses defined in ``--yaml``')
-
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
-
-    mandatory.add_argument('--yaml',
-                        dest='yaml',
-                        required=True,
-                        help=help_yaml + ex_yaml)
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
-
+    desc = "DESCRIPTION: Create/update all Netbox virtual_ip_addresses (vm IPs)"
+    desc += " defined in ``--yaml``"
+    parser = argparse.ArgumentParser(description=desc)
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
+    mandatory.add_argument(
+        "--yaml", dest="yaml", required=True, help=f"{help_yaml} {ex_yaml}"
+    )
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
     return parser.parse_args()
+
 
 cfg = get_parser()
 info = load_yaml(cfg.yaml)
 nb = netbox()
-for key in info['virtual_interfaces']:
-    if 'ip4' not in info['virtual_interfaces'][key]:
+for key in info["virtual_interfaces"]:
+    if "ip4" not in info["virtual_interfaces"][key]:
         continue
-    print('---')
-    ip_address_dict = make_ip_address_dict(info['ip4_addresses'], info['virtual_interfaces'][key])
+    print("---")
+    ip_address_dict = make_ip_address_dict(
+        info["ip4_addresses"], info["virtual_interfaces"][key]
+    )
     vip = VirtualIpAddress(nb, ip_address_dict)
     vip.create_or_update()
--- scripts/virtual_ip_address_delete_all.py
+++ scripts/virtual_ip_address_delete_all.py
@@ -1,43 +1,44 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: virtual_ip_address_delete_all.py
-Description: Delete all virtual_ip_addresses (those belonging to virtual machines) defined in ``--yaml``
-'''
-OUR_VERSION = 100
+Description: Delete all virtual_ip_addresses (vm IPs) defined in ``--yaml``
+"""
 import argparse
-
 from netbox_tools.common import netbox, load_yaml
 from netbox_tools.virtual_ip_address import VirtualIpAddress
 
+OUR_VERSION = 101
+
+
 def get_parser():
-    help_yaml = 'YAML file in which ip_address information can be found.'
+    """
+    return an argparse parser object
+    """
+    help_yaml = "YAML file in which ip_address information can be found."
 
-    ex_prefix_ = ' Example: '
-    ex_yaml = '{} --yaml ./info.yml'.format(ex_prefix_)
+    ex_prefix = " Example: "
+    ex_yaml = f"{ex_prefix} --yaml ./info.yml"
 
-    parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Delete all virtual_ip_addresses (those belonging to virtual machines) defined in ``--yaml``')
-
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
-
-    mandatory.add_argument('--yaml',
-                        dest='yaml',
-                        required=True,
-                        help=help_yaml + ex_yaml)
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
-
+    description = (
+        "DESCRIPTION: Delete all virtual_ip_addresses (vm IPs) defined in ``--yaml``"
+    )
+    parser = argparse.ArgumentParser(description=description)
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
+    mandatory.add_argument(
+        "--yaml", dest="yaml", required=True, help=f"{help_yaml} {ex_yaml}"
+    )
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
     return parser.parse_args()
+
 
 cfg = get_parser()
 info = load_yaml(cfg.yaml)
 nb = netbox()
-print('---')
-for key in info['virtual_interfaces']:
-    if 'ip4' not in info['virtual_interfaces'][key]:
+print("---")
+for key in info["virtual_interfaces"]:
+    if "ip4" not in info["virtual_interfaces"][key]:
         continue
-    vip = VirtualIpAddress(nb, info['virtual_interfaces'][key])
-    vip.delete()
+    vip_obj = VirtualIpAddress(nb, info["virtual_interfaces"][key])
+    vip_obj.delete()

f8c355758b38720bedd82aa08baee70df85443f9
Allen Robel <arobel@cisco.com>
2023-01-05 14:38:02 -1000

    run thru pylint/black

--- scripts/location_delete.py
+++ scripts/location_delete.py
@@ -1,40 +1,52 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: location_delete.py
 Description: Delete location ``--location`` from netbox
-'''
-OUR_VERSION = 102
+"""
 import argparse
-
 from netbox_tools.common import netbox
 from netbox_tools.location import Location
 
+OUR_VERSION = 103
+
+
 def get_parser():
-    help_location = 'Name of the location to delete.'
+    """
+    return an argparse parser object
+    """
+    help_location = "Name of the location to delete."
 
-    ex_prefix = ' Example: '
-    ex_location = '{} --location mylocation'.format(ex_prefix)
+    ex_prefix = " Example: "
+    ex_location = f"{ex_prefix} --location mylocation"
 
-    parser = argparse.ArgumentParser(description='DESCRIPTION: Delete location --location from netbox')
+    parser = argparse.ArgumentParser(
+        description="DESCRIPTION: Delete location --location from netbox"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
 
-    mandatory.add_argument('--location',
-                        dest='location',
-                        required=True,
-                        help=help_location + ex_location)
+    mandatory.add_argument(
+        "--location",
+        dest="location",
+        required=True,
+        help=f"{help_location} {ex_location}",
+    )
 
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
+
 def get_info():
-    info = dict()
-    info['name'] = cfg.location
+    """
+    return dictionary containing args expected by Location
+    """
+    info = {}
+    info["name"] = cfg.location
     return info
+
 
 cfg = get_parser()
 nb = netbox()

934df2d8f3b3ed135ae2ceab9fb9d5dd18b0a7d9
Allen Robel <arobel@cisco.com>
2023-01-06 10:16:12 -1000

    run thru pylint/black

--- scripts/tag_create_update_all.py
+++ scripts/tag_create_update_all.py
@@ -1,39 +1,40 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: tag_create_update_all.py
 Description: Create/update tags defined in ``--yaml``
-'''
-OUR_VERSION = 102
+"""
 import argparse
 from netbox_tools.common import netbox, load_yaml
 from netbox_tools.tag import Tag
 
-def get_parser():
-    help_yaml = 'YAML file containing device type information.'
+OUR_VERSION = 103
 
-    ex_prefix     = 'Example: '
-    ex_yaml = '{} --yaml ./tags.yml'.format(ex_prefix)
+
+def get_parser():
+    """
+    return an argparse parser object
+    """
+    help_yaml = "YAML file containing device type information."
+
+    ex_prefix = "Example: "
+    ex_yaml = f"{ex_prefix} --yaml ./tags.yml"
 
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Create or update tags in Netbox from information in a YAML file')
-
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
-
-    mandatory.add_argument('--yaml',
-                        dest='yaml',
-                        required=True,
-                        help=help_yaml + ex_yaml)
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
-
+        description="DESCRIPTION: Create/update tags defined in ``--yaml``"
+    )
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
+    mandatory.add_argument(
+        "--yaml", dest="yaml", required=True, help=f"{help_yaml} {ex_yaml}"
+    )
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
     return parser.parse_args()
+
 
 cfg = get_parser()
 nb = netbox()
 info = load_yaml(cfg.yaml)
-for key in info['tags']:
-    tag = Tag(nb, info['tags'][key])
+for key in info["tags"]:
+    tag = Tag(nb, info["tags"][key])
     tag.create_or_update()

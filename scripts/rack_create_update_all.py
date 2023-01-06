7ccb314fc46b997aba5e440299838c57b365242f
Allen Robel <arobel@cisco.com>
2023-01-05 14:54:46 -1000

    run thru pylint/black

--- scripts/rack_create_update_all.py
+++ scripts/rack_create_update_all.py
@@ -1,39 +1,43 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: rack_create_update_all.py
-Description: Create/update racks defined in ``--yaml``
-'''
-OUR_VERSION = 102
+Description: Create/update Netbox racks defined in ``--yaml``
+"""
 import argparse
 from netbox_tools.common import netbox, load_yaml
 from netbox_tools.rack import Rack
 
-def get_parser():
-    help_yaml = 'YAML file containing device type information.'
+OUR_VERSION = 103
 
-    ex_prefix     = 'Example: '
-    ex_yaml = '{} --yaml ./racks.yml'.format(ex_prefix)
+
+def get_parser():
+    """
+    return an argparse parser object
+    """
+    help_yaml = "YAML file containing device type information."
+
+    ex_prefix = "Example: "
+    ex_yaml = f"{ex_prefix} --yaml ./racks.yml"
 
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Create or update racks in Netbox from information in a YAML file')
+        description="DESCRIPTION: Create/update Netbox racks defined in ``--yaml`"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
+    mandatory.add_argument(
+        "--yaml", dest="yaml", required=True, help=f"{help_yaml} {ex_yaml}"
+    )
 
-    mandatory.add_argument('--yaml',
-                        dest='yaml',
-                        required=True,
-                        help=help_yaml + ex_yaml)
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
+
 
 cfg = get_parser()
 nb = netbox()
 info = load_yaml(cfg.yaml)
-for key in info['racks']:
-    rack = Rack(nb, info['racks'][key])
+for key in info["racks"]:
+    rack = Rack(nb, info["racks"][key])
     rack.create_or_update()

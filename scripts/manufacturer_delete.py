0a294495b1445ddbea233e3fa2a09fbc1453f47a
Allen Robel <arobel@cisco.com>
2023-01-05 14:51:32 -1000

    run thru pylint/black

--- scripts/manufacturer_delete.py
+++ scripts/manufacturer_delete.py
@@ -1,40 +1,52 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: manufacturer_delete.py
-Description: Delete manufacturer ``--manufacturer``
-'''
-OUR_VERSION = 102
+Description: Delete Netbox manufacturer ``--manufacturer``
+"""
 import argparse
-
 from netbox_tools.common import netbox
 from netbox_tools.manufacturer import Manufacturer
 
+OUR_VERSION = 103
+
+
 def get_parser():
-    help_manufacturer = 'Name of the manufacturer to delete.'
+    """
+    return an argparse parser object
+    """
+    help_manufacturer = "Name of the manufacturer to delete."
 
-    ex_prefix = ' Example: '
-    ex_manufacturer = '{} --manufacturer mymanufacturer'.format(ex_prefix)
+    ex_prefix = " Example: "
+    ex_manufacturer = f"{ex_prefix} --manufacturer mymanufacturer"
 
-    parser = argparse.ArgumentParser(description='DESCRIPTION: Netbox: Delete a manufacturer')
+    parser = argparse.ArgumentParser(
+        description="DESCRIPTION: Delete Netbox manufacturer ``--manufacturer``"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
 
-    mandatory.add_argument('--manufacturer',
-                        dest='manufacturer',
-                        required=True,
-                        help=help_manufacturer + ex_manufacturer)
+    mandatory.add_argument(
+        "--manufacturer",
+        dest="manufacturer",
+        required=True,
+        help=f"{help_manufacturer} {ex_manufacturer}",
+    )
 
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
+
 def get_info():
-    info = dict()
-    info['name'] = cfg.manufacturer
+    """
+    return dictionary containing args expected by Manufacturer
+    """
+    info = {}
+    info["name"] = cfg.manufacturer
     return info
+
 
 cfg = get_parser()
 nb = netbox()

52ea440617ca86bddeaf582a7e52d10ef9c1a46a
Allen Robel <arobel@cisco.com>
2023-01-05 14:46:47 -1000

    run thru pylint/black, minor refactor

--- scripts/manufacturer_create_update.py
+++ scripts/manufacturer_create_update.py
@@ -1,39 +1,52 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: manufacturer_create_update.py
-Description: Create/update manufacturer ``--manufacturer``
-'''
-OUR_VERSION = 102
+Description: Create/update Netbox manufacturer ``--manufacturer``
+"""
 import argparse
-from netbox_tools.common import netbox, load_yaml
+from netbox_tools.common import netbox
 from netbox_tools.manufacturer import Manufacturer
 
+OUR_VERSION = 103
+
+
 def get_parser():
-    help_manufacturer = 'Who makes this product'
-    ex_prefix     = 'Example: '
-    ex_manufacturer = '{} --manufacturer cisco'.format(ex_prefix)
+    """
+    return an argparse parser object
+    """
+    help_manufacturer = "Who makes this product"
+    ex_prefix = "Example: "
+    ex_manufacturer = f"{ex_prefix} --manufacturer cisco"
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Create or update a Netbox manufacturer')
+        description="DESCRIPTION: Create/update Netbox manufacturer ``--manufacturer``"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    optional   = parser.add_argument_group(title='OPTIONAL SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
 
-    mandatory.add_argument('--manufacturer',
-                        dest='manufacturer',
-                        required=True,
-                        help=help_manufacturer + ex_manufacturer)
+    mandatory.add_argument(
+        "--manufacturer",
+        dest="manufacturer",
+        required=True,
+        help=f"{help_manufacturer} {ex_manufacturer}",
+    )
 
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
+
+def get_info():
+    """
+    return dictionary containing args expected by Manufacturer
+    """
+    info = {}
+    info["name"] = cfg.manufacturer
+    return info
+
+
 cfg = get_parser()
 nb = netbox()
-
-info = dict()
-info['name'] = cfg.manufacturer
-
-m = Manufacturer(nb, info)
+m = Manufacturer(nb, get_info())
 m.create_or_update()

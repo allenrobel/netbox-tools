4c553a0fc7649320a85813daa3a29d0239af4cfd
Allen Robel <arobel@cisco.com>
2023-01-05 14:29:38 -1000

    run thru pylint/black

--- scripts/location_create_update_all.py
+++ scripts/location_create_update_all.py
@@ -1,39 +1,43 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: location_create_update_all.py
-Description: Create/update locations defined in ``--yaml``.
-'''
-OUR_VERSION = 102
+Description: Create/update Netbox locations defined in ``--yaml``.
+"""
 import argparse
 from netbox_tools.common import netbox, load_yaml
 from netbox_tools.location import Location
 
-def get_parser():
-    help_yaml = 'YAML file containing device type information.'
+OUR_VERSION = 103
 
-    ex_prefix     = 'Example: '
-    ex_yaml = '{} --yaml ./locations.yml'.format(ex_prefix)
+
+def get_parser():
+    """
+    return an argparse parser object
+    """
+    help_yaml = "YAML file containing device type information."
+
+    ex_prefix = "Example: "
+    ex_yaml = f"{ex_prefix} --yaml ./locations.yml"
 
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Create or update locations in Netbox from information in a YAML file')
+        description="DESCRIPTION: Create/update Netbox locations defined in ``--yaml``"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
+    mandatory.add_argument(
+        "--yaml", dest="yaml", required=True, help=f"{help_yaml} {ex_yaml}"
+    )
 
-    mandatory.add_argument('--yaml',
-                        dest='yaml',
-                        required=True,
-                        help=help_yaml + ex_yaml)
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
+
 
 cfg = get_parser()
 nb = netbox()
 info = load_yaml(cfg.yaml)
-for key in info['locations']:
-    location = Location(nb, info['locations'][key])
+for key in info["locations"]:
+    location = Location(nb, info["locations"][key])
     location.create_or_update()

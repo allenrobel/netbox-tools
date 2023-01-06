b787aa211498dadd6fc825f6938d8dfd12f5d023
Allen Robel <arobel@cisco.com>
2023-01-06 08:42:46 -1000

    run thru pylint/black

--- scripts/roles_print.py
+++ scripts/roles_print.py
@@ -1,41 +1,90 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: roles_print.py
-Description: Display information about all device roles
-'''
-OUR_VERSION = 103
+Description: Display information about all Netbox device roles
+"""
 import argparse
+import sys
 from netbox_tools.common import netbox
 from netbox_tools.colors import rgb_to_color
 
+OUR_VERSION = 103
+
+
 def get_parser():
+    """
+    return an argparse parser object
+    """
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: retrieve and print information about all device roles')
+        description="DESCRIPTION: Display information about all Netbox device roles"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
 
+
 def get_fmt():
-    return '{:>5} {:<18} {:>12} {:<6} {:<15} {:<7} {:<30}'
+    """
+    return a format string
+    """
+    fmt = "{id:>5} {role_name:<18} {device_count:>12} {rgb:<6} {color:<15} {vm_role:<7}"
+    fmt += " {description:<30}"
+    return fmt
+
 
 def print_device_roles():
+    """
+    print column values
+    """
     roles = nb.dcim.device_roles.all()
-    if roles == None:
-        exit()
+    if roles is None:
+        sys.exit(0)
     fmt = get_fmt()
     for role in roles:
-        print(fmt.format(role.id, role.name, role.device_count, role.color, rgb_to_color(role.color), role.vm_role, role.description))
+        print(
+            fmt.format(
+                id=role.id,
+                role_name=role.name,
+                device_count=role.device_count,
+                rgb=role.color,
+                color=rgb_to_color(role.color),
+                vm_role=role.vm_role,
+                description=role.description,
+            )
+        )
+
 
 def print_headers():
+    """
+    print column headers
+    """
     fmt = get_fmt()
-    print(fmt.format('id', 'role_name', 'device_count', 'rgb', 'color', 'vm_role', 'description'))
-    print(fmt.format('-' * 5, '-' * 18, '-' * 12, '-' * 6, '-' * 15, '-' * 7, '-' * 30))
+    print(
+        fmt.format(
+            id="id",
+            role_name="role_name",
+            device_count="device_count",
+            rgb="rgb",
+            color="color",
+            vm_role="vm_role",
+            description="description",
+        )
+    )
+    print(
+        fmt.format(
+            id="-" * 5,
+            role_name="-" * 18,
+            device_count="-" * 12,
+            rgb="-" * 6,
+            color="-" * 15,
+            vm_role="-" * 7,
+            description="-" * 30,
+        )
+    )
+
 
 cfg = get_parser()
 nb = netbox()

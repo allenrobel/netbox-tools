3956080c8e74b4a3ee14986bdb8092eab35b6da2
Allen Robel <arobel@cisco.com>
2023-01-05 14:41:44 -1000

    run thru pylint/black

--- scripts/manufacturer_create_update_all.py
+++ scripts/manufacturer_create_update_all.py
@@ -1,39 +1,44 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: manufacturer_create_update_all.py
-Description: Create/update manufacturers defined in ``--yaml``
-'''
-OUR_VERSION = 102
+Description: Create/update Netbox manufacturers defined in ``--yaml``
+"""
 import argparse
 from netbox_tools.common import netbox, load_yaml
 from netbox_tools.manufacturer import Manufacturer
 
-def get_parser():
-    help_yaml = 'YAML file containing device type information.'
+OUR_VERSION = 103
 
-    ex_prefix     = 'Example: '
-    ex_yaml = '{} --yaml ./manufacturers.yml'.format(ex_prefix)
+
+def get_parser():
+    """
+    return an argparse parser object
+    """
+    help_yaml = "YAML file containing device type information."
+
+    ex_prefix = "Example: "
+    ex_yaml = f"{ex_prefix} --yaml ./manufacturers.yml"
 
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Create or update manufacturers in Netbox from information in a YAML file')
+        description="DESCRIPTION: Create/update Netbox manufacturers defined in ``--yaml``"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
 
-    mandatory.add_argument('--yaml',
-                        dest='yaml',
-                        required=True,
-                        help=help_yaml + ex_yaml)
+    mandatory.add_argument(
+        "--yaml", dest="yaml", required=True, help=f"{help_yaml} {ex_yaml}"
+    )
 
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
 
     return parser.parse_args()
+
 
 cfg = get_parser()
 nb = netbox()
 info = load_yaml(cfg.yaml)
-for key in info['manufacturers']:
-    manufacturer = Manufacturer(nb, info['manufacturers'][key])
+for key in info["manufacturers"]:
+    manufacturer = Manufacturer(nb, info["manufacturers"][key])
     manufacturer.create_or_update()

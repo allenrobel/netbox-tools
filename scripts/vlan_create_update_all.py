16bc57cc649cad25c6aad50adcbd4c0f47b9d73b
Allen Robel <arobel@cisco.com>
2023-01-06 14:24:24 -1000

    run thru pylint/black

--- scripts/vlan_create_update_all.py
+++ scripts/vlan_create_update_all.py
@@ -1,42 +1,52 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: vlan_create_update_all.py
 Description: Create/update vlans defined in ``--yaml``
-'''
-OUR_VERSION = 102
+"""
 import argparse
+from inspect import stack
+import sys
 from netbox_tools.common import netbox, load_yaml
 from netbox_tools.vlan import Vlan
 
-def get_parser():
-    help_yaml = 'YAML file containing Vlan information.'
+OUR_VERSION = 103
 
-    ex_prefix     = 'Example: '
-    ex_yaml = '{} --yaml ./vlans.yml'.format(ex_prefix)
+
+def get_parser():
+    """
+    return an argparse parser object
+    """
+    help_yaml = "YAML file containing Vlan information."
+
+    ex_prefix = "Example: "
+    ex_yaml = f"{ex_prefix} --yaml ./vlans.yml"
 
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Create/update vlans in Netbox from information in a YAML file')
-
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
-
-    mandatory.add_argument('--yaml',
-                        dest='yaml',
-                        required=True,
-                        help=help_yaml + ex_yaml)
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
-
+        description="DESCRIPTION: Create/update vlans defined in ``--yaml``"
+    )
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
+    mandatory.add_argument(
+        "--yaml", dest="yaml", required=True, help=f"{help_yaml} {ex_yaml}"
+    )
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
     return parser.parse_args()
 
+
+def log(*args):
+    """
+    simple logger
+    """
+    print(f"{stack()[1].function}(v{OUR_VERSION}): {' '.join(args)}")
+
+
 cfg = get_parser()
-nb = netbox()
+netbox_obj = netbox()
 info = load_yaml(cfg.yaml)
-if 'vlans' not in info:
-    print('exiting. no vlans to process in {}'.format(cfg.yaml))
-    exit(1)
-for key in info['vlans']:
-    vlan = Vlan(nb, info['vlans'][key])
+if "vlans" not in info:
+    log(f"Exiting. No vlans to process in {cfg.yaml}")
+    sys.exit(1)
+for key in info["vlans"]:
+    vlan = Vlan(netbox_obj, info["vlans"][key])
     vlan.create_or_update()

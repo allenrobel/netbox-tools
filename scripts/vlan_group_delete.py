6bc62df0f5c5a413560d08c0487c1f1b33d9cc20
Allen Robel <arobel@cisco.com>
2023-01-06 14:55:53 -1000

    run thru pylint/black

--- scripts/vlan_group_delete.py
+++ scripts/vlan_group_delete.py
@@ -1,42 +1,47 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: vlan_group_delete.py
 Description: Delete vlan_group ``--vlan_group``
-'''
-OUR_VERSION = 103
+"""
 import argparse
-
 from netbox_tools.common import netbox
 from netbox_tools.vlan_group import VlanGroup
 
+OUR_VERSION = 104
+
+
 def get_parser():
-    help_vlan_group = 'Name of the VlanGroup to delete.'
+    """
+    return an argparse parser object
+    """
+    help_vlan_group = "Name of the VlanGroup to delete."
 
-    ex_prefix = ' Example: '
-    ex_vlan_group = '{} --vlan_group AdminServers'.format(ex_prefix)
-
-    parser = argparse.ArgumentParser(description='DESCRIPTION: Netbox: Delete a VlanGroup')
-
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
-
-    mandatory.add_argument('--vlan_group',
-                        dest='vlan_group',
-                        required=True,
-                        help=help_vlan_group + ex_vlan_group)
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
-
+    ex_prefix = " Example: "
+    ex_vlan_group = f"{ex_prefix} --vlan_group AdminServers"
+    parser = argparse.ArgumentParser(
+        description="DESCRIPTION: Netbox: Delete a VlanGroup"
+    )
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
+    mandatory.add_argument(
+        "--vlan_group",
+        dest="vlan_group",
+        required=True,
+        help=f"{help_vlan_group} {ex_vlan_group}",
+    )
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
     return parser.parse_args()
 
+
 def get_info():
-    info = dict()
-    info['vlan_group'] = cfg.vlan_group
+    """
+    return dictionary containing args expected by VlanGroup
+    """
+    info = {}
+    info["vlan_group"] = cfg.vlan_group
     return info
 
+
 cfg = get_parser()
-nb = netbox()
-d = VlanGroup(nb, get_info())
-d.delete()
+VlanGroup(netbox(), get_info()).delete()

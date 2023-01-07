06d75335950339cd0310e6a6a922ea9a77553725
Allen Robel <arobel@cisco.com>
2023-01-06 14:32:48 -1000

    run thru pylint/black

--- scripts/vlan_group_create_update_all.py
+++ scripts/vlan_group_create_update_all.py
@@ -1,39 +1,40 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: vlan_group_create_update_all.py
 Description: Create/update vlan_groups defined in ``--yaml``
-'''
-OUR_VERSION = 102
+"""
 import argparse
 from netbox_tools.common import netbox, load_yaml
 from netbox_tools.vlan_group import VlanGroup
 
-def get_parser():
-    help_yaml = 'YAML file containing VlanGroup information.'
+OUR_VERSION = 103
 
-    ex_prefix     = 'Example: '
-    ex_yaml = '{} --yaml ./vlan_groups.yml'.format(ex_prefix)
+
+def get_parser():
+    """
+    return an argparse parser object
+    """
+    help_yaml = "YAML file containing VlanGroup information."
+
+    ex_prefix = "Example: "
+    ex_yaml = f"{ex_prefix} --yaml ./vlans.yml"
 
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Create or update VlanGroups in Netbox from information in a YAML file')
-
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
-
-    mandatory.add_argument('--yaml',
-                        dest='yaml',
-                        required=True,
-                        help=help_yaml + ex_yaml)
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
-
+        description="DESCRIPTION: Create/update vlan_groups defined in ``--yaml``"
+    )
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
+    mandatory.add_argument(
+        "--yaml", dest="yaml", required=True, help=f"{help_yaml} {ex_yaml}"
+    )
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
     return parser.parse_args()
 
+
 cfg = get_parser()
-nb = netbox()
+netbox_obj = netbox()
 info = load_yaml(cfg.yaml)
-for key in info['vlan_groups']:
-    vlan_group = VlanGroup(nb, info['vlan_groups'][key])
+for key in info["vlan_groups"]:
+    vlan_group = VlanGroup(netbox_obj, info["vlan_groups"][key])
     vlan_group.create_or_update()

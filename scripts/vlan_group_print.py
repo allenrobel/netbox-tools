3541e4ed652d90a01df6f8a6292d99f108515ae4
Allen Robel <arobel@cisco.com>
2023-01-06 15:16:25 -1000

    run thru pylint/black, refactor

--- scripts/vlan_group_print.py
+++ scripts/vlan_group_print.py
@@ -1,76 +1,148 @@
 #!/usr/bin/env python3
-'''
+"""
 Name: vlan_group_print.py
-Description: Display information about vlan_group ``--vlan_group``
-'''
-OUR_VERSION = 103
+Description: Display information about Netbox vlan_group ``--vlan_group``
+"""
 import argparse
+from inspect import stack
 import json
+import sys
 from netbox_tools.common import netbox
 
-def get_parser():
-    help_detail = 'Optional. If present, print detailed info about VlanGroup.'
-    help_vlan_group = 'Name of the VlanGroup.'
+OUR_VERSION = 104
 
-    ex_prefix     = 'Example: '
-    ex_detail = '{} --detail'.format(ex_prefix)
-    ex_vlan_group = '{} --vlan_group AdminServers'.format(ex_prefix)
+
+def get_parser():
+    """
+    return an argparse parser object
+    """
+    help_detail = "Optional. If present, print detailed info about VlanGroup."
+    help_vlan_group = "Name of the VlanGroup."
+
+    ex_prefix = "Example: "
+    ex_detail = f"{ex_prefix} --detail"
+    ex_vlan_group = "{ex_prefix} --vlan_group AdminServers"
 
     parser = argparse.ArgumentParser(
-            description='DESCRIPTION: Display information about a VlanGroup')
+        description="DESCRIPTION: Display information about Netbox vlan_group ``--vlan_group``"
+    )
 
-    mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+    mandatory = parser.add_argument_group(title="MANDATORY SCRIPT ARGS")
+    default = parser.add_argument_group(title="DEFAULT SCRIPT ARGS")
 
-    default.add_argument('--detail',
-                        dest='detail',
-                        required=False,
-                        default=False,
-                        action='store_true',
-                        help=help_detail + ex_detail)
-    mandatory.add_argument('--vlan_group',
-                        dest='vlan_group',
-                        required=True,
-                        help=help_vlan_group + ex_vlan_group)
-
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {}'.format(OUR_VERSION))
-
+    default.add_argument(
+        "--detail",
+        dest="detail",
+        required=False,
+        default=False,
+        action="store_true",
+        help=f"{help_detail} {ex_detail}",
+    )
+    mandatory.add_argument(
+        "--vlan_group",
+        dest="vlan_group",
+        required=True,
+        help=f"{help_vlan_group} {ex_vlan_group}",
+    )
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {OUR_VERSION}"
+    )
     return parser.parse_args()
 
-def error():
-    vlan_groups = list()
-    items = nb.ipam.vlan_groups.all()
+
+def log(*args):
+    """
+    simple logger
+    """
+    print(f"{stack()[1].function}(v{OUR_VERSION}): {' '.join(args)}")
+
+
+def error(netbox_object):
+    """
+    Print a helpful error message and exit.
+    """
+    vlan_groups = []
+    items = netbox_object.ipam.vlan_groups.all()
     for item in items:
         vlan_groups.append(item.name)
-    print('vlan_group {} does not exist in netbox.  Valid vlan_groups: {}'.format(cfg.vlan_group, ', '.join(vlan_groups)))
-    exit(1)
+    msg = f"vlan_group {cfg.vlan_group} does not exist in netbox."
+    msg += f" Valid vlan_groups: {', '.join(vlan_groups)}"
+    log(msg)
+    sys.exit(1)
 
-def get_vlan_group():
-    vlan_group = nb.ipam.vlan_groups.get(name=cfg.vlan_group)
-    if vlan_group == None:
-        error()
+
+def get_vlan_group(netbox_object):
+    """
+    Return a vlan_group object matching user's search.
+    If object does not exist, call error()
+    """
+    vlan_group = netbox_object.ipam.vlan_groups.get(name=cfg.vlan_group)
+    if vlan_group is None:
+        error(netbox_object)
     return vlan_group
 
-def print_detail():
+
+def print_detail(vlan_group):
+    """
+    Print detailed info about vlan_group
+    """
     pretty = json.dumps(dict(vlan_group), indent=4, sort_keys=True)
     print(pretty)
 
+
 def print_headers():
-    print(fmt.format('id',    'name',   'min_vid', 'max_vid', 'description'))
-    print(fmt.format('-' * 5, '-' * 15, '-' * 7,    '-' * 7,   '-' * 25))
+    """
+    print column headers
+    """
+    print(
+        get_fmt().format(
+            id="id",
+            name="name",
+            min_vid="min_vid",
+            max_vid="max_vid",
+            description="description",
+        )
+    )
+    print(
+        get_fmt().format(
+            id="-" * 5,
+            name="-" * 15,
+            min_vid="-" * 7,
+            max_vid="-" * 7,
+            description="-" * 25,
+        )
+    )
+
+
+def print_values(vlan_group):
+    """
+    print column values
+    """
+    print(
+        get_fmt().format(
+            id=vlan_group.id,
+            name=vlan_group.name,
+            min_vid=vlan_group.min_vid,
+            max_vid=vlan_group.max_vid,
+            description=str(vlan_group.description),
+        )
+    )
+
+
+def get_fmt():
+    """
+    return a format string
+    """
+    return "{id:>5} {name:<15} {min_vid:>7} {max_vid:>7} {description:<25}"
 
 
 cfg = get_parser()
-nb = netbox()
-vlan_group = get_vlan_group()
-
-fmt = '{:>5} {:>15} {:>7} {:>7} {:>25}'
+netbox_obj = netbox()
+vlan_group_obj = get_vlan_group(netbox_obj)
 
 if cfg.detail:
-    print_detail()
-    exit()
+    print_detail(vlan_group_obj)
+    sys.exit(0)
 
 print_headers()
-print(fmt.format(vlan_group.id, vlan_group.name, vlan_group.min_vid, vlan_group.max_vid, str(vlan_group.description)))
+print_values(vlan_group_obj)
